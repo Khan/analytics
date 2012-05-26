d1f84890186bf9ac076a9cbf3d2bf7987fa9f476
Ben Komalo <benkomalo@gmail.com>
2012-05-25 20:18:22 -0700

    Make it so the server can accept all requests
    Summary:
    Debug mode can only accept requests from localhosts - make it
    configurable at least
    
    Reviewers: david
    
    Reviewed By: david
    
    Differential Revision: http://phabricator.khanacademy.org/D108

--- webapps/dashboards/main.py
+++ webapps/dashboards/main.py
@@ -5,10 +5,11 @@ and a daily report of videos watched. It will house more dashboards for
 fundamental metrics we want to track.
 """
 
+import optparse
+
 import flask
 
 app = flask.Flask(__name__)
-app.debug = True  # Show stacktraces and auto-refresh.
 
 
 @app.route('/')
@@ -27,5 +28,26 @@ def exercises_dashboard():
     return flask.render_template('daily-ex-stats.html')
 
 
+def main():
+    parser = optparse.OptionParser()
+    parser.add_option("-d", "--debug", action="store_true", dest="debug",
+                      help="Whether to run in debug mode "
+                           "(only accessible by localhost and autoreloads)")
+    parser.add_option("-p", "--port", type="int", default=-1,
+                      help="The port to run on (defaults to 5000 for debug, "
+                           "else defaults to 80)")
+    options, _ = parser.parse_args()
+
+    app.debug = options.debug
+    port = options.port
+    if options.debug:
+        if port == -1:
+            port = 5000
+        app.run(port=port)
+    else:
+        if port == -1:
+            port = 80
+        app.run(host='0.0.0.0', port=port)
+
 if __name__ == '__main__':
-    app.run()
+    main()
