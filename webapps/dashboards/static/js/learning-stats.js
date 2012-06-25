b04dc23e2bd362b9c6c83821f4a4084de1bf8342
David Hu <davidhu91@gmail.com>
2012-06-25 13:44:49 -0700

    refactor learning dashboard to use backbone.js in preparation for showing multiple series
    Summary:
    Up next: Use handlebars to render the HTML for specifying a series (those select
    boxes), and then actually show multiple series in JS.
    
    Sorry about the code dump, but this is mostly a moving-things-around diff.
    
    Test Plan: Ran manually.
    
    Reviewers: benkomalo, tom
    
    Reviewed By: benkomalo
    
    CC: jace, yunfangjuan
    
    Differential Revision: http://phabricator.khanacademy.org/D288

--- webapps/dashboards/static/js/learning-stats.js
+++ webapps/dashboards/static/js/learning-stats.js
@@ -13,146 +13,265 @@ var BASE_COLLLECTION_URL = BASE_STAT_SERVER_URL +
     "report/weekly_learning_stats/";
 
 
-/** @type {HighCharts.Chart|null} */
-var learningGraph = null;
-
-
 /**
- * Every batch request needs a unique identifier since JSONP requests can't be
- * cancelled, so we check in the handler whether this is the latest request.
- * @type {number}
+ * A single data series to show on a chart.
  */
-var resultsRequestCount = 0;
+var Series = Backbone.Model.extend({
 
+    defaults: {
+        results: [],
+        numCalls: 0,
+        batchSize: 1000,
+        requestCount: 0
+    },
 
-/**
- * Script entry-point called on DOM ready.
- */
-var init = function init() {
-    // Pre-load the "number of stacks completed" select box
-    $("#stacks-select").append(_.map(_.range(1, 21), function(num) {
-        return $("<option value=" + num + ">").text("exactly " + num)[0];
-    }));
+    /**
+     * Reset to prepare for a new chain of batch calls.
+     */
+    reset: function() {
+        this.set("results", []);
+        this.set("numCalls", 0);
+    },
 
-    learningGraph = createChart();
-    addEventHandlers();
-    refresh();
-    getTopics();
-};
+    /**
+     * Get a single batch of data from the server, and get more if necessary.
+     */
+    fetchResults: function(url, params) {
 
-
-// TODO(david): Use backbone?
-var addEventHandlers = function addEventHandlers() {
-    $("#stacks-select").change(refresh);
-    $("#topics-select").change(refresh);
-};
-
-
-/**
- * Get topic IDs to generate learning curves for and populate select box.
- */
-var getTopics = function() {
-    // TODO(david): Filter out pseudo-topic "any"
-    $.get("/db/learning_stats_topics", function(data) {
-        var options = _.map(data["topics"], function(topic) {
-            return $("<option>").text(topic)[0];
-        });
-        $("#topics-select").append(options);
-    });
-};
-
-
-/**
- * Regenerate the chart from user-set controls on the dashboard by asking server
- * for new data and then rendering.
- */
-var refresh = function refresh() {
-
-    var $loadingBar = $("#efficiency-chart-loading .bar"),
-        $chart = $("#efficiency-chart");
-    $loadingBar.css("width", "10%").parent().show();
-    $chart.css("opacity", 0.5);
-
-    var numStacks = $("#stacks-select").val();
-    var topic = $("#topics-select option:selected").val();
-
-    // TODO(david): More permanent database, and design summary table with date
-    //     partitions.
-    var url = BASE_COLLLECTION_URL + "_find?callback=?";
-
-    // TODO(david): Batch up requests
-    var criteria = {
-        num_problems_done: numStacks === "any" ? { $lte: 160 } : numStacks * 8,
-        topic: topic,
-        start_dt: '2012-06-13'  // TODO(david): Support date range selection
-    };
-
-    var batchSize = 1000;
-    var params = {
-        criteria: JSON.stringify(criteria),
-        batch_size: batchSize,
-        fields: JSON.stringify({
-            card_number: true,
-            num_deltas: true,
-            sum_deltas: true
-        })
-    };
-
-    var results = [];
-    var numCalls = 0;
-
-    // TODO(david): Caching. Share code with video stats.
-    function getResults(url, params) {
+        var self = this;
+        this.set("requestCount", this.get("requestCount") + 1);
 
         AjaxCache.getJson(url, params, _.bind(function(requestCount, data) {
 
-            // Another request has taken place, abort this one
-            if (requestCount !== resultsRequestCount) {
+            // A new batch request has been initiated, abort this one
+            if (requestCount !== self.get("requestCount")) {
                 return;
             }
 
             var dataResults = data["results"];
             if (dataResults && dataResults.length) {
 
-                // Update the chart from new data
+                // Update with new data
+                var results = self.get("results");
                 results = results.concat(dataResults);
                 results = groupByCardNumber(results);
-                updateChart(results, learningGraph);
-                updateSampleStats(results);
+                self.set("results", results);
 
             }
 
-            // Update UI elements
-            numCalls++;
-            var fakedProgress = 1 - Math.pow(0.66, numCalls);
-            $loadingBar.css("width", fakedProgress.toFixed(2) * 100 + "%");
-            $chart.css("opacity", 1.0);
+            if (dataResults && dataResults.length === self.get("batchSize")) {
 
-            if (dataResults && dataResults.length === batchSize) {
-
-                // Prepare and another batch of results
+                // There's probably more; fetch another batch of results
                 var moreUrl = BASE_COLLLECTION_URL + "_more?callback=?";
                 var moreParams = {
-                    batch_size: batchSize,
+                    batch_size: self.get("batchSize"),
                     id: data["id"],
-                    callNum_: numCalls
+                    callNum_: self.get("numCalls")
                 };
-                getResults(moreUrl, moreParams);
+                self.fetchResults(moreUrl, moreParams);
 
             } else {
 
-                // Finished loading, update UI.
-                $loadingBar.parent().hide();
+                self.trigger("allResultsLoaded");
 
             }
 
-        }, null, ++resultsRequestCount));
+            self.set("numCalls", self.get("numCalls") + 1);
+
+        }, null, this.get("requestCount")));
+
     }
 
-    getResults(url, params);
-    updateSampleStats(results);
+});
 
-};
+
+/**
+ * View of a data series.
+ * TODO(david): Change IDs to classes to support multiple series views.
+ */
+var SeriesView = Backbone.View.extend({
+
+    // TODO(david): Going to be changed to support multiple views
+    el: "body",
+
+    events: {
+        "change #stacks-select": "refresh",
+        "change #topics-select": "refresh"
+    },
+
+    initialize: function(options) {
+
+        this.chart = options.chart;
+
+        // Pre-load the "number of stacks completed" select box
+        $("#stacks-select").append(_.map(_.range(1, 21), function(num) {
+            return $("<option value=" + num + ">").text("exactly " + num)[0];
+        }));
+
+        this.populateTopics();
+
+        this.model
+            .bind("change:results", this.updateSeries, this)
+            .bind("change:numCalls", this.updateProgress, this)
+            .bind("allResultsLoaded", function() {
+                $("#efficiency-chart-loading").hide();
+            });
+
+        this.refresh();
+
+    },
+
+    render: function() {
+        // TODO(david): Render a handlebars template here
+        return this;
+    },
+
+    /**
+     * Get topic IDs to generate learning curves for and populate select box.
+     */
+    populateTopics: function() {
+        // TODO(david): Filter out pseudo-topic "any"
+        AjaxCache.getJson("/db/learning_stats_topics", {}, function(data) {
+            var options = _.map(data["topics"], function(topic) {
+                return $("<option>").text(topic)[0];
+            });
+            $("#topics-select").append(options);
+        });
+    },
+
+    /**
+     * Ask the server for new data from user-set controls on the dashboard.
+     */
+    refresh: function() {
+
+        $("#efficiency-chart-loading").show();
+
+        var numStacks = $("#stacks-select").val();
+        var topic = $("#topics-select option:selected").val();
+
+        // TODO(david): More permanent database, and design summary table with
+        //     date partitions.
+        var url = BASE_COLLLECTION_URL + "_find?callback=?";
+
+        // TODO(david): Batch up requests
+        var criteria = {
+            num_problems_done: numStacks === "any" ? { $lte: 160 } :
+                numStacks * 8,
+            topic: topic,
+            start_dt: '2012-06-13'  // TODO(david): Support date range selection
+        };
+
+        var params = {
+            criteria: JSON.stringify(criteria),
+            batch_size: this.model.get("batchSize"),
+            fields: JSON.stringify({
+                card_number: true,
+                num_deltas: true,
+                sum_deltas: true
+            })
+        };
+
+        this.model.reset();
+        this.model.fetchResults(url, params);
+
+    },
+
+    updateProgress: function() {
+        var fakedProgress = 1 - Math.pow(0.66, this.model.get("numCalls"));
+        fakedProgress = Math.max(0.1, fakedProgress);
+        $("#efficiency-chart-loading .bar")
+                .css("width", fakedProgress.toFixed(2) * 100 + "%");
+    },
+
+    /**
+     * Update UI elements that show data series info, such as the graph and
+     * sample counter.
+     */
+    updateSeries: function() {
+
+        var results = this.model.get("results");
+
+        var incrementalGains = _.chain(results)
+            .groupByCardNumber()
+            .sortBy(function(row) { return +row["card_number"]; })
+            .map(function(row, index) {
+                return row["sum_deltas"] / row["num_deltas"];
+            })
+            .value();
+
+        var accumulatedGains = _.reduce(incrementalGains,
+                function(accum, delta) {
+            return accum.concat([_.last(accum) + delta]);
+        }, [0]);
+
+        this.chart.series[0].setData(accumulatedGains);
+        this.chart.series[1].setData(incrementalGains);
+
+        // TODO(david): Show # of distinct users and error bounds
+        var totalDeltas = _.reduce(results, function(accum, row) {
+            return accum + +row["num_deltas"];
+        }, 0);
+        $("#total-deltas").text(totalDeltas);
+
+    }
+
+});
+
+
+/**
+ * Chart of learning efficiency, as measured by incremental accuracy deltas.
+ */
+var EfficiencyChartView = Backbone.View.extend({
+
+    initialize: function() {
+        var chart = this.createChart();
+        var series = new Series();
+        var seriesView = new SeriesView({ model: series, chart: chart });
+    },
+
+    /**
+     * Create learning curve HighCharts graph.
+     * @return {HighCharts.Chart}
+     */
+    createChart: function() {
+        // TODO(david): Overlay charts on different segments for easy comparison
+        //     (eg. num_problems_done)
+        // TODO(david): Dynamically generate labels and titles
+        var chartOptions = {
+            chart: {
+                renderTo: "efficiency-chart"
+            },
+            series: [{
+                data: [],
+                type: "areaspline",
+                name: "Accumulated gain in accuracy",
+                // TODO(david): Bootstrap from 1st card % correct?
+                pointStart: 0
+            }, {
+                data: [],
+                type: "spline",
+                name: "Incremental gain in accuracy",
+                pointStart: 1
+            }],
+            title: {
+                text: "Gain in accuracy over number of cards done"
+            },
+            yAxis: {
+                title: { text: "Gain in accuracy" },
+            },
+            xAxis: {
+                title: { text: "Card Number" }
+            },
+            credits: { enabled: false }
+        };
+
+        var chart = new Highcharts.Chart(chartOptions);
+        chart.series[1].hide();  // Hide "incremental gains" series by default
+
+        return chart;
+    }
+
+});
 
 
 /**
@@ -182,91 +301,15 @@ var groupByCardNumber = function groupByCardNumber(results) {
 };
 
 
-/**
- * Create learning curve HighCharts graph.
- * @return {HighCharts.Chart}
- */
-var createChart = function createLearningGraph() {
-    // TODO(david): Overlay charts on different segments for easy comparison
-    //     (eg. num_problems_done)
-    // TODO(david): Dynamically generate labels and titles
-    var chartOptions = {
-        chart: {
-            renderTo: "efficiency-chart"
-        },
-        series: [{
-            data: [],
-            type: "areaspline",
-            name: "Accumulated gain in accuracy",
-            pointStart: 0  // TODO(david): Bootstrap from 1st card % correct?
-        }, {
-            data: [],
-            type: "spline",
-            name: "Incremental gain in accuracy",
-            pointStart: 1
-        }],
-        title: {
-            text: "Gain in accuracy over number of cards done"
-        },
-        yAxis: {
-            title: { text: "Gain in accuracy" },
-        },
-        xAxis: {
-            title: { text: "Card Number" }
-        },
-        credits: { enabled: false }
-    };
-
-    var chart = new Highcharts.Chart(chartOptions);
-    chart.series[1].hide();  // Hide "incremental gains" series by default
-
-    return chart;
-};
-
-
-/**
- * Update learning chart data from fresh results from the server.
- * @param {Array.<Object>} results Rows from the reducer summary table in mongo.
- * @param {HighCharts.Chart} chart Chart to update.
- */
-var updateChart = function updateChart(results, chart) {
-    var incrementalGains = _.chain(results)
-        .groupByCardNumber()
-        .sortBy(function(row) { return +row["card_number"]; })
-        .map(function(row, index) {
-            return row["sum_deltas"] / row["num_deltas"];
-        })
-        .value();
-
-    var accumulatedGains = _.reduce(incrementalGains, function(accum, delta) {
-        return accum.concat([_.last(accum) + delta]);
-    }, [0]);
-
-    chart.series[0].setData(accumulatedGains);
-    chart.series[1].setData(incrementalGains);
-};
-
-
-/**
- * Update the counter of how many incremental gains this sample uses.
- * @param {Array.<Object>} results Rows from the reducer summary table in mongo.
- */
-var updateSampleStats = function updateSampleStats(results) {
-    // TODO(david): Show # of distinct users and error bounds
-    var totalDeltas = _.reduce(results, function(accum, row) {
-        return accum + +row["num_deltas"];
-    }, 0);
-    $("#total-deltas").text(totalDeltas);
-};
-
-
 // Add utility functions to underscore for convenience in chaining
 _.mixin({
     groupByCardNumber: groupByCardNumber
 });
 
 
-$(init);
+$(function() {
+    var dashboard = new EfficiencyChartView();
+});
 
 
 })();
