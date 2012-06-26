Khan/analytics

commit 0fac10aa857ab1494e90195c2787d199b57126bf

Step 1 in import flow for video correlation matrix.
Summary:
Changes the matrix to be a video key to video key, instead of title,
since that's ultimately what we need to send to GAE.

Still lots of TODO's here, but this basically introduces the "pruner"
that sorts and limits the number of videos each video is scored with.

Reviewers: josh, jace

Reviewed By: jace

CC: yunfangjuan

Differential Revision: http://phabricator.khanacademy.org/D293

map_reduce/hive/video_recommendation.q
@@ -1,9 +1,10 @@
--- Hiave script that does video recommendations 
+-- Hive script that computes video to video correlations.
+--
 -- 4 parameters should be supplied
--- suffix: table postfix for the output summary tables 
--- start_dt: start date stamp YYYY-mm-dd
--- end_dt: exclusive end date stamp YYYY-mm-dd
--- branch: code branch to run the recommend reducer: 'dev' or 'prod'
+--   suffix: table postfix for the output summary tables
+--   start_dt: start date stamp YYYY-mm-dd
+--   end_dt: exclusive end date stamp YYYY-mm-dd
+--   branch: code branch to run the recommend reducer: 'dev' or 'prod'
 
 -- Getting (user, video, time_completed) tuples
 DROP TABLE user_vid_completion_${suffix};
@@ -16,7 +17,7 @@ SELECT get_json_object(VideoLog.json, '$.user'),
        get_json_object(VideoLog.json, '$.video'),
        get_json_object(VideoLog.json, '$.video_title'),
        MIN(get_json_object(VideoLog.json, '$.time_watched'))
-FROM VideoLog 
+FROM VideoLog
 WHERE get_json_object(VideoLog.json, '$.is_video_completed') = 'true' AND
   dt >= '${start_dt}' AND dt < '${end_dt}'
 GROUP BY get_json_object(VideoLog.json, '$.user'),
@@ -34,45 +35,72 @@ SELECT vid_title, COUNT(1) FROM user_vid_completion_${suffix}
 GROUP BY vid_title;
 
 -- Generating the co-ocurrence matrix
-DROP TABLE video_cooccurrence_${suffix}; 
+DROP TABLE video_cooccurrence_${suffix};
 CREATE EXTERNAL TABLE video_coocurrence_${suffix}(
-  vid1_title STRING, vid2_title STRING, 
+  vid1_key STRING, vid2_key STRING,
   preceed_cnt INT, succeed_cnt INT)
 LOCATION 's3://ka-mapreduce/tmp/video_cooccurrence_${suffix}';
 
 ADD FILE s3://ka-mapreduce/code/${branch}/py/video_recommendation_reducer.py;
 FROM (
-  FROM ( 
-    FROM user_vid_completion_${suffix} 
-    SELECT user, vid_title, completion_time 
-    CLUSTER BY USER) map_out 
+  FROM (
+    FROM user_vid_completion_${suffix}
+    SELECT user, vid_key, completion_time
+    CLUSTER BY USER) map_out
   SELECT TRANSFORM(map_out.*)
-  USING 'video_recommendation_reducer.py' 
-  AS vid1_title, vid2_title, preceed_cnt, succeed_cnt) red_out 
+  USING 'video_recommendation_reducer.py'
+  AS vid1_key, vid2_key, preceed_cnt, succeed_cnt) red_out
 INSERT OVERWRITE TABLE video_coocurrence_${suffix}
-SELECT red_out.vid1_title, red_out.vid2_title, 
+SELECT red_out.vid1_key, red_out.vid2_key,
   SUM(red_out.preceed_cnt), SUM(red_out.succeed_cnt)
-GROUP BY red_out.vid1_title, red_out.vid2_title;
+GROUP BY red_out.vid1_key, red_out.vid2_key;
 
 
--- Join the occurences matrix and the video count table together 
+-- Join the occurences matrix and the video count table together
 
 DROP TABLE video_cooccurrence_cnt_${suffix};
 CREATE EXTERNAL TABLE video_cooccurrence_cnt_${suffix}(
-  vid1_title STRING, vid2_title STRING, 
-  preceed_cnt INT, succeed_cnt INT, 
+  vid1_key STRING, vid2_key STRING,
+  preceed_cnt INT, succeed_cnt INT,
   vid1_cnt INT, vid2_cnt INT)
-LOCATION 's3://ka-mapreduce/tmp/video_cooccurrence_cnt_${suffix}'; 
+LOCATION 's3://ka-mapreduce/tmp/video_cooccurrence_cnt_${suffix}';
 
-INSERT OVERWRITE TABLE video_cooccurrence_cnt_${suffix} 
-SELECT b.vid1_title, b.vid2_title, b.preceed_cnt, b.succeed_cnt,
-       a.cnt, c.cnt 
-FROM video_completion_cnt_${suffix} a JOIN 
-  video_coocurrence_${suffix} b ON (a.vid_title = b.vid1_title) JOIN
-  video_completion_cnt_${suffix} c ON (c.vid_title = b.vid2_title);
+INSERT OVERWRITE TABLE video_cooccurrence_cnt_${suffix}
+SELECT
+  b.vid1_key, b.vid2_key,
+  b.preceed_cnt, b.succeed_cnt,
+  a.cnt, c.cnt
+FROM video_completion_cnt_${suffix} a
+JOIN video_coocurrence_${suffix} b ON (a.vid_title = b.vid1_key)
+JOIN video_completion_cnt_${suffix} c ON (c.vid_title = b.vid2_key);
 
 -- Example queries to see the top 10 video rec for a specific video
 -- SELECT *, (preceed_cnt + succeed_cnt)/sqrt(vid1_cnt)/sqrt(vid2_cnt) AS
---  similarity FROM video_cooccurrence_cnt_${suffix} 
--- WHERE vid1_title = "${video}"
+--  similarity FROM video_cooccurrence_cnt_${suffix}
+-- WHERE vid1_key = "${video}"
 -- ORDER BY similarity DESC LIMIT 10;
+
+-- TODO(benkomalo): separate this out to a different file for a dedicated
+-- import job flow.
+
+-- Prune the results for importing to production
+DROP TABLE video_cooccurrence_cnt_pruned_${suffix};
+CREATE EXTERNAL TABLE video_cooccurrence_cnt_pruned_${suffix}(
+  vid1_key STRING, vid2_key STRING,
+  preceed_cnt INT, succeed_cnt INT,
+  vid1_cnt INT, vid2_cnt INT)
+LOCATION 's3://ka-mapreduce/tmp/video_cooccurrence_cnt_pruned_${suffix}';
+
+ADD FILE s3://ka-mapreduce/code/${branch}/py/video_recommendation_pruner.py;
+
+FROM (
+  FROM video_cooccurrence_cnt_${suffix}
+  SELECT *
+  CLUSTER BY vid1_title
+) unpruned_data
+SELECT TRANSFORM(unpruned_data.*)
+USING 'video_recommendation_pruner.py'
+INSERT OVERWRITE TABLE video_coocurrence_cnt_pruned_${suffix};
+SELECT *;
+
+

map_reduce/py/video_recommendation_reducer.py
@@ -1,7 +1,4 @@
-#!/usr/bin/python
-
-import itertools
-import sys
+#!/usr/bin/env python
 
 """The reduce step of our first MapReduce, allowing us to find how many users
 completed both video i and video j for any i and j,
@@ -32,6 +29,11 @@ happening, see:
 https://sites.google.com/a/khanacademy.org/forge/technical/data_n/collaborative-filtering-with-emr
 
 """
+
+
+import itertools
+import sys
+
 
 _out = sys.stdout  # For testing purposes
 _in = sys.stdin  # For testing purposes

map_reduce/py/video_recommendation_pruner.py
@@ -1,0 +1,95 @@
+#!/usr/bin/env python
+
+"""A custom reducer for pruning a video-to-video co-occurence matrix.
+
+This accepts entries for a video co-occurence matrix, clustered by
+the first video of each video pair, and emits the MAX_BEST most
+correlated videos for each video.
+
+Input expected in tab-delimited lines containing matrix entries of the form:
+
+(video_i, video_j, preceed_count, succeed_count, videoi_count, videoj_count)
+
+Where:
+preceed_count - the number of times a user watched videoi, then watched videoj
+succeed_count - the number of times a user watched videoj, then watched videoi
+videoi_count - the number of times videoi was watched
+videoj_count - the number of times videoj was watched
+
+Output:
+Emits the same lines of the input, but only the top MAX_BEST for each video.
+
+"""
+
+
+import heapq
+import math
+import sys
+
+
+DELIMITER = '\t'
+MAX_BEST = 20
+_IN = sys.stdin  # For unit testing.
+_OUT = sys.stdout
+
+
+def emit_best_pairs(video_key, scored_pairs):
+    """Emit a video and its computed best pairs of videos.
+    Arguments:
+        video_key - a string for the video key these pairs belong to
+        scored_pairs - a list of tuples of (score, line_parts),
+            where line_parts is to be joined to create an output line
+    """
+    if not video_key:
+        return
+
+    best_pairs = heapq.nlargest(MAX_BEST, scored_pairs)
+    # TODO(benkomalo): should we just emit the score for convenience?
+    for _, parts in best_pairs:
+        _OUT.write(DELIMITER.join(parts) + '\n')
+
+
+def main():
+    last_video = None
+    scored_pairs = []
+
+    for line in _IN:
+        if not line:
+            # EOF
+            break
+
+        parts = line.rstrip().split(DELIMITER)
+
+        if len(parts) != 6:
+            # TODO(benkomalo): error handling.
+            continue
+
+        (vid1_key, vid2_key,
+         preceed_count, succeed_count,
+         video1_count, video2_count) = parts
+
+        try: 
+            preceed_count = int(preceed_count)
+            succeed_count = int(succeed_count)
+            video1_count = int(video1_count)
+            video2_count = int(video2_count)
+        except ValueError:
+            # Some of the values were invalid - deal with it.
+            # TODO(benkomalo): error handling.
+            continue
+
+        if last_video != vid1_key:
+            emit_best_pairs(last_video, scored_pairs)
+            scored_pairs = []
+
+        # TODO(benkomalo): do we have to re-normalize after pruning?
+        score = (preceed_count + succeed_count) / math.sqrt(video2_count)
+        scored_pairs.append((score, parts))
+        last_video = vid1_key
+
+    emit_best_pairs(last_video, scored_pairs)
+
+
+if __name__ == '__main__':
+    main()
+

map_reduce/py/video_recommendation_pruner_test.py
@@ -1,0 +1,80 @@
+import StringIO
+import unittest
+
+import video_recommendation_pruner
+
+
+class PrunerTest(unittest.TestCase):
+
+    def setUp(self):
+        self.orig_in = video_recommendation_pruner._IN
+        self.orig_out = video_recommendation_pruner._OUT
+        
+        video_recommendation_pruner._IN = []
+        video_recommendation_pruner._OUT = StringIO.StringIO()
+
+    def tearDown(self):
+        video_recommendation_pruner._IN = self.orig_in
+        video_recommendation_pruner._OUT = self.orig_out
+
+    def run_reducer(self):
+        video_recommendation_pruner.main()
+
+    def fake_input(self, lines_in_parts):
+        for line in lines_in_parts:
+            video_recommendation_pruner._IN.append('\t'.join(line))
+            
+    def get_output_in_lists(self):
+        return video_recommendation_pruner._OUT.getvalue().rstrip().split("\n")
+
+    def assertOutput(self, expected_output_in_parts):
+        expected_output = map(lambda t: '\t'.join(t),
+                              expected_output_in_parts)
+        actual_output = self.get_output_in_lists()
+        
+        # Order doesn't strictly matter, as long as it clusters by the first
+        # video key
+        self.assertEquals(set(expected_output),
+                          set(actual_output))
+        
+    def test_single_pair(self):
+        self.fake_input([["vid1", "vid2", "0", "1", "1", "1"]])
+
+        self.run_reducer()
+        self.assertOutput([["vid1", "vid2", "0", "1", "1", "1"]])
+
+    def test_one_video_paired_with_lots(self):
+        input = []
+        expected = []
+        for i in range(1, 101):
+            # Video i is named "othervid{i}" and watched i times.
+            # after "vid1"
+            video_i = [
+                "vid1",
+                "othervid%s" % i,
+                "0", str(i),
+                "1", "100"
+            ]
+            input.append(video_i)
+
+            # We expect the last MAX_BEST to win
+            if i + video_recommendation_pruner.MAX_BEST > 100:
+                expected.append(video_i)
+                
+        self.fake_input(input)
+        self.run_reducer()
+        self.assertOutput(expected)
+
+    def test_two_videos(self):
+        input = [["vid1", "othervid3", "0", "1", "2", "1"],
+                 ["vid1", "othervid4", "0", "1", "2", "1"],
+                 ["vid2", "othervid3", "0", "1", "2", "1"],
+                 ["vid2", "othervid4", "0", "1", "2", "1"],
+                ]
+        self.fake_input(input)
+        self.run_reducer()
+        self.assertOutput(input)
+
+
+if __name__ == '__main__':
+    unittest.main()

commit d36ea213a82695d49f2ff951d8e9755c8154e3ba

Fix issues with video matrix generation.
Summary: This fixes a bunch of issues introduced in 0fac10aa857.

Test Plan:
actually ran it on Hive this time.
a little embarrassed at some of these errors. :)

Reviewers: josh, jace

Reviewed By: jace

Differential Revision: http://phabricator.khanacademy.org/D300

map_reduce/hive/video_recommendation.q
@@ -27,12 +27,12 @@ GROUP BY get_json_object(VideoLog.json, '$.user'),
 -- Getting frequency counts from the user_vid_completion table
 DROP TABLE video_completion_cnt_${suffix};
 CREATE EXTERNAL TABLE video_completion_cnt_${suffix}(
-  vid_title STRING, cnt INT)
+  vid_key STRING, cnt INT)
 LOCATION 's3://ka-mapreduce/tmp/video_completion_cnt_${suffix}';
 
 INSERT OVERWRITE TABLE video_completion_cnt_${suffix}
-SELECT vid_title, COUNT(1) FROM user_vid_completion_${suffix}
-GROUP BY vid_title;
+SELECT vid_key, COUNT(1) FROM user_vid_completion_${suffix}
+GROUP BY vid_key;
 
 -- Generating the co-ocurrence matrix
 DROP TABLE video_cooccurrence_${suffix};
@@ -46,7 +46,7 @@ FROM (
   FROM (
     FROM user_vid_completion_${suffix}
     SELECT user, vid_key, completion_time
-    CLUSTER BY USER) map_out
+    CLUSTER BY user) map_out
   SELECT TRANSFORM(map_out.*)
   USING 'video_recommendation_reducer.py'
   AS vid1_key, vid2_key, preceed_cnt, succeed_cnt) red_out
@@ -71,8 +71,8 @@ SELECT
   b.preceed_cnt, b.succeed_cnt,
   a.cnt, c.cnt
 FROM video_completion_cnt_${suffix} a
-JOIN video_coocurrence_${suffix} b ON (a.vid_title = b.vid1_key)
-JOIN video_completion_cnt_${suffix} c ON (c.vid_title = b.vid2_key);
+JOIN video_coocurrence_${suffix} b ON (a.vid_key = b.vid1_key)
+JOIN video_completion_cnt_${suffix} c ON (c.vid_key = b.vid2_key);
 
 -- Example queries to see the top 10 video rec for a specific video
 -- SELECT *, (preceed_cnt + succeed_cnt)/sqrt(vid1_cnt)/sqrt(vid2_cnt) AS
@@ -94,13 +94,16 @@ LOCATION 's3://ka-mapreduce/tmp/video_cooccurrence_cnt_pruned_${suffix}';
 ADD FILE s3://ka-mapreduce/code/${branch}/py/video_recommendation_pruner.py;
 
 FROM (
-  FROM video_cooccurrence_cnt_${suffix}
-  SELECT *
-  CLUSTER BY vid1_title
-) unpruned_data
-SELECT TRANSFORM(unpruned_data.*)
-USING 'video_recommendation_pruner.py'
-INSERT OVERWRITE TABLE video_coocurrence_cnt_pruned_${suffix};
-SELECT *;
+  FROM (
+    FROM video_cooccurrence_cnt_${suffix}
+    SELECT *
+    CLUSTER BY vid1_key
+  ) unpruned_data
+  SELECT TRANSFORM(unpruned_data.*)
+  USING 'video_recommendation_pruner.py'
+  AS vid1_key, vid2_key, preceed_cnt, succeed_cnt, vid1_cnt, vid2_cnt
+) pruned_data
+INSERT OVERWRITE TABLE video_cooccurrence_cnt_pruned_${suffix}
+SELECT pruned_data.*;
 
 

map_reduce/py/video_recommendation_reducer.py
@@ -71,8 +71,10 @@ def main():
     videos = []
 
     for line in _in:
-        line = line.rstrip().split("\t")
+        if not line:
+            continue
 
+        line = line.rstrip().split("\t")
         if len(line) != 3:
             sys.stderr.write("Malformed input: '%s'!\n" % "\t".join(line))
             return