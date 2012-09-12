faf3370d47f1e65dca04ecc441830b364072e7e3
Yun-Fang Juan <yunfang@khanacademy.org>
2012-09-12 10:53:03 -0700

    adding the utility for running weekly/monthly reports
    Summary:
    -- regular_report.json : running regular reports
    -- run_report.sh
    will update aws-config cron after i check this in.
    
    Test Plan: ran it on analytics machine
    
    Reviewers: jace, benkomalo
    
    Differential Revision: http://phabricator.khanacademy.org/D732

---
+++ src/run_report.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# This script take in an argument to know the duration and then generates
+# command to run report_generator.py
+
+# Get duration
+if [ "$1" = "month" ]; then 
+  end_dt=$(date "+%Y-%m-01")
+  start_dt=$(date --date=${end_dt}-1month "+%Y-%m-01")
+else if [ "$1" = "week" ]; then
+  end_dt=$(date "+%Y-%m-%d")
+  start_dt=$(date --date=${end_dt}-1week "+%Y-%m-%d")
+else
+  echo "not a right duration '$1'"
+  exit 1
+fi
+fi
+end_dt_inclusive=$(date --date=${end_dt}-1day "+%Y-%m-%d")
+
+# Generate and run the command
+current_dir=`dirname $0`
+command="${current_dir}/../src/report_generator.py -c ${current_dir}/../cfg/daily_report.json '<duration>=$1' '<start_dt>=${start_dt}' '<end_dt>=${end_dt}' '<end_dt_inclusive>=${end_dt_inclusive}'"
+echo $command
+#$command 
