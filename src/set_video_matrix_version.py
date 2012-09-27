0141ca709959f797344993f720f1b704df05f3c0
Ben Komalo <benkomalo@gmail.com>
2012-09-27 09:42:24 -0700

    Add version support to video rec scripts.
    Summary:
    This adds the version datestamp parameter when exporting the data from
    Hive to GAE. It also introduces a script to set a version live.
    
    Test Plan:
    Locally ran
    ./video_matrix_exporter.py 's3://ka-mapreduce/tmp/video_suggestions_pruned_causal_20120601_20120919/' 'ordered completions'
    Then ran
    ./set_video_matrix_version.py 'ordered completions' '2012-09-22T03:17:28'
    
    and ensured both succeeded with 200's
    
    (Note I had a src/oauth_util/devconsts.py to make them run against localhost)
    
    Reviewers: jace, alpert
    
    Reviewed By: alpert
    
    Differential Revision: http://phabricator.khanacademy.org/D782

---
+++ src/set_video_matrix_version.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+"""Sets the version for the video recommendations data on production."""
+
+import json
+import sys
+import oauth_util.consts
+import oauth_util.fetch_url as oauth_fetcher
+
+
+def main(score_type, version):
+    print "About to modify video recommendations data:"
+    print "  target: ", oauth_util.consts.SERVER_URL
+    print "  score_type: ", score_type
+    print "  version: ", version
+    confirm = raw_input("Proceed? (y/[n]): ")
+
+    if confirm.lower() in ['y', 'yes']:
+        # This will throw on 4xx, 5xx
+        resp = oauth_fetcher.fetch_url(
+                '/api/v1/dev/videorec_version', {
+                    'score_type': score_type,
+                    'version': version,
+                })
+        resp = json.loads(resp)
+        print "Success!"
+        print "To revert back, run:"
+        print "set_video_matrix_version.py '%s' '%s'" % (
+                score_type, resp['previous'])
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print >>sys.stderr, (
+            "Usage:\n"
+            "    set_video_matrix_version.py <score_type> <version>")
+        sys.exit(1)
+
+    main(sys.argv[1], sys.argv[2])
+
