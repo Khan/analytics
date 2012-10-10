585d0648e7045068b3905158eedc46586a567961
Craig Silverstein <csilvers@khanacademy.org>
2012-10-10 15:43:35 -0700

    api_cpu_ms is gone from appengine as of v1.7.1, so remove it here too.
    Summary:
    We removed it from our logs (in commit 1ed539edd155 of stable), so all
    new analysis won't see it.
    
    They didn't do a great job of documenting this fact, but it was
    confirmed in
       https://enterprise.google.com/supportcenter/#Case/ACTING_ACCOUNT_ID=001600000
    
    Test Plan: Crossed fingers
    
    Reviewers: chris
    
    Reviewed By: chris
    
    Differential Revision: http://phabricator.khanacademy.org/D834

--- map_reduce/py/raw_log_to_request_log_mapper.py
+++ map_reduce/py/raw_log_to_request_log_mapper.py
@@ -16,18 +16,18 @@ logs in the following format:
 
 A user-facing request:
 
-91.174.232.10 - chris [24/Jul/2012:17:00:09 -0700] "GET /assets/images/thumbnails/Rothko-13.jpg HTTP/1.1" 200 572 "http://smarthistory.khanacademy.org/" "Mozilla/5.0" "smarthistory.khanacademy.org" ms=65 cpu_ms=35 api_cpu_ms=10 cpm_usd=0.000001 pending_ms=0 instance=00c61b117c5f1f26699563074cdd44e841096e
+91.174.232.10 - chris [24/Jul/2012:17:00:09 -0700] "GET /assets/images/thumbnails/Rothko-13.jpg HTTP/1.1" 200 572 "http://smarthistory.khanacademy.org/" "Mozilla/5.0" "smarthistory.khanacademy.org" ms=65 cpu_ms=35 cpm_usd=0.000001 pending_ms=0 instance=00c61b117c5f1f26699563074cdd44e841096e
 
 A task queue request, initiated by App Engine:
 
-0.1.0.2 - - [31/Jul/2012:17:00:09 -0700] "POST /_ah/queue/deferred_problemlog HTTP/1.1" 200 84 "http://www.khanacademy.org/api/v1/user/exercises/converting_between_point_slope_and_slope_intercept/problems/1/attempt" "AppEngine-Google; (+http://code.google.com/appengine)" "www.khanacademy.org" ms=88 cpu_ms=300 api_cpu_ms=275 cpm_usd=0.000007 queue_name=problem-log-queue task_name=10459794276075119660 pending_ms=0 instance=00c61b117cca420ac067602b717789e7aec8ca
+0.1.0.2 - - [31/Jul/2012:17:00:09 -0700] "POST /_ah/queue/deferred_problemlog HTTP/1.1" 200 84 "http://www.khanacademy.org/api/v1/user/exercises/converting_between_point_slope_and_slope_intercept/problems/1/attempt" "AppEngine-Google; (+http://code.google.com/appengine)" "www.khanacademy.org" ms=88 cpu_ms=300 cpm_usd=0.000007 queue_name=problem-log-queue task_name=10459794276075119660 pending_ms=0 instance=00c61b117cca420ac067602b717789e7aec8ca
 
-Additional logs for testing. Note that api_cpu_ms and instance may be "None",
+Additional logs for testing. Note that instance may be "None",
 and cpm_usd is occasionally, unexpectedly, 0.000000:
 
-122.11.36.130 - - [06/Oct/2012:16:00:09 -0700] "GET /api/v1/user/topic/precache/addition-subtraction/e/addition_1?casing=camel HTTP/1.1" 200 2421 "http://www.khanacademy.org/math/arithmetic/addition-subtraction/v/basic-addition" "Mozilla/5.0 (iPad; CPU OS 5_1_1 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9B206 Safari/7534.48.3" "www.khanacademy.org" ms=263 cpu_ms=415 api_cpu_ms=None cpm_usd=0.000000 pending_ms=0 instance=00c61b117c29add96b8c86824f3be8d9b22d5537
+122.11.36.130 - - [06/Oct/2012:16:00:09 -0700] "GET /api/v1/user/topic/precache/addition-subtraction/e/addition_1?casing=camel HTTP/1.1" 200 2421 "http://www.khanacademy.org/math/arithmetic/addition-subtraction/v/basic-addition" "Mozilla/5.0 (iPad; CPU OS 5_1_1 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9B206 Safari/7534.48.3" "www.khanacademy.org" ms=263 cpu_ms=415 cpm_usd=0.000000 pending_ms=0 instance=00c61b117c29add96b8c86824f3be8d9b22d5537
 
-174.211.15.119 - - [06/Oct/2012:16:00:09 -0700] "GET /images/featured-actions/campbells-soup.png HTTP/1.1" 204 154518 "http://www.khanacademy.org/" "Mozilla/5.0 (iPhone; CPU iPhone OS 5_0_1 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A405 Safari/7534.48.3" "www.khanacademy.org" ms=19 cpu_ms=0 api_cpu_ms=None cpm_usd=0.000017 pending_ms=0 instance=None
+174.211.15.119 - - [06/Oct/2012:16:00:09 -0700] "GET /images/featured-actions/campbells-soup.png HTTP/1.1" 204 154518 "http://www.khanacademy.org/" "Mozilla/5.0 (iPhone; CPU iPhone OS 5_0_1 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A405 Safari/7534.48.3" "www.khanacademy.org" ms=19 cpu_ms=0 cpm_usd=0.000017 pending_ms=0 instance=None
 
 
 Output is one record per line. Each record contains tab-separated
@@ -44,7 +44,6 @@ values whose fields are, in order with examples from the above logs:
   referer         # http://smarthistory.khanacademy.org
   ms              # 65
   cpu_ms          # 35
-  api_cpu_ms      # 10
   cpm_usd         # 0.000001
   queue_name      # problem-log-queue (or the empty string if n/a)
   pending_ms      # 0
@@ -89,7 +88,8 @@ _LOG_MATCHER = re.compile(r"""
     "(?P<host>[^"]+)"\s
     ms=(?P<ms>\d+)\s
     cpu_ms=(?P<cpu_ms>\d+)\s
-    api_cpu_ms=(?P<api_cpu_ms>\d+|None)\s
+    # Old logs (before Oct 2012) used to have an api_cpu_ms field.
+    (?:api_cpu_ms=\S+\s)?
     cpm_usd=(?P<cpm_usd>\S+)\s
     (?:queue_name=(?P<queue_name>\S+)\s)?
     (?:task_name=(?P<task_name>\S+)\s)?
@@ -101,7 +101,7 @@ _LOG_MATCHER = re.compile(r"""
 # We emit just these fields, and in this order.
 _FIELDS_TO_KEEP = ('ip', 'user', 'time_stamp', 'method', 'url',
                    'protocol', 'status', 'bytes', 'referer',
-                   'ms', 'cpu_ms', 'api_cpu_ms', 'cpm_usd', 'queue_name',
+                   'ms', 'cpu_ms', 'cpm_usd', 'queue_name',
                    'pending_ms')
 
 
@@ -225,11 +225,6 @@ def main(input_file, route_regexps):
         fields = {}
         for f in _FIELDS_TO_KEEP:
             fields[f] = match.group(f) or ''
-
-        # api_cpu_ms may be "None" if not provided by App Engine. The
-        # equivalent in Hive is an empty field.
-        if fields['api_cpu_ms'] == 'None':
-            fields['api_cpu_ms'] = ''
 
         # Get a copy of the field-values in _FIELDS_TO_KEEP order.
         sorted_fields = sorted(fields.items(),
