d67a4e11ef430be4d50f144c72225af2a9ec0216
Craig Silverstein <csilvers@khanacademy.org>
2012-10-25 16:47:07 -0700

    Fix the code to have 4-space indents.
    Summary:
    I am reluctant to make such lint-only changes, since it will make it
    more difficult to merge in changes to these files in the future, but
    I'm already hacking on these files enough that changes are going to be
    difficult to incorporate.  And I can't take all the phabricator
    complaining anymore.
    
    Get rid of an unused import.
    
    Test Plan: Ran run_webpagetest.py and it worked like before.
    
    Reviewers: chris
    
    Reviewed By: chris
    
    Differential Revision: http://phabricator.khanacademy.org/D873

--- src/webpagetest/wpt_batch.py
+++ src/webpagetest/wpt_batch.py
@@ -35,221 +35,222 @@ import wpt_batch_lib
 
 
 def BuildFileName(url):
-  """Construct the file name from a given URL.
+    """Construct the file name from a given URL.
 
-  Args:
-    url: the given URL
+    Args:
+      url: the given URL
 
-  Returns:
-    filename: the constructed file name
-  """
-  filename = url.strip('\r\n\t \\/')
-  filename = filename.replace('http://', '')
-  filename = filename.replace(':', '_')
-  filename = filename.replace('/', '_')
-  filename = filename.replace('\\', '_')
-  filename = filename.replace('%', '_')
-  return filename
+    Returns:
+      filename: the constructed file name
+    """
+    filename = url.strip('\r\n\t \\/')
+    filename = filename.replace('http://', '')
+    filename = filename.replace(':', '_')
+    filename = filename.replace('/', '_')
+    filename = filename.replace('\\', '_')
+    filename = filename.replace('%', '_')
+    return filename
 
 
 def SaveTestResult(output_dir, url, test_id, content):
-  """Save the result of a test into a file on disk.
+    """Save the result of a test into a file on disk.
 
-  Args:
-    output_dir: the directory to save the result
-    url: the associated URL
-    test_id: the ID of the test
-    content: the string of test result
+    Args:
+      output_dir: the directory to save the result
+      url: the associated URL
+      test_id: the ID of the test
+      content: the string of test result
 
-  Returns:
-    None
-  """
-  filename = BuildFileName(url)
-  filename = '%s/%s.%s.xml' % (output_dir.rstrip('/'), filename, test_id)
-  output = open(filename, 'wb')
-  output.write(content)
-  output.close()
+    Returns:
+      None
+    """
+    filename = BuildFileName(url)
+    filename = '%s/%s.%s.xml' % (output_dir.rstrip('/'), filename, test_id)
+    output = open(filename, 'wb')
+    output.write(content)
+    output.close()
 
 
 def StartBatch(options, verbose=False):
-  """Set off some urls to be processed, and return the id-to-url map."""
-  test_params = {'f': 'xml',
-                 'private': 1,
-                 'priority': 6,
-                 'video': options.video,
-                 'fvonly': options.fvonly,
-                 'runs': options.runs,
-                 'location': options.location,
-                 'mv': options.mv
-                }
-  if options.connectivity == 'custom':
-    test_params['bwOut'] = options.bwup,
-    test_params['bwIn'] = options.bwdown,
-    test_params['latency'] = options.latency,
-    test_params['plr'] = options.plr,
-    test_params['location'] = options.location + '.custom'
-  else:
-    test_params['location'] = options.location + '.' + options.connectivity
+    """Set off some urls to be processed, and return the id-to-url map."""
+    test_params = {'f': 'xml',
+                   'private': 1,
+                   'priority': 6,
+                   'video': options.video,
+                   'fvonly': options.fvonly,
+                   'runs': options.runs,
+                   'location': options.location,
+                   'mv': options.mv
+                  }
+    if options.connectivity == 'custom':
+        test_params['bwOut'] = options.bwup,
+        test_params['bwIn'] = options.bwdown,
+        test_params['latency'] = options.latency,
+        test_params['plr'] = options.plr,
+        test_params['location'] = options.location + '.custom'
+    else:
+        test_params['location'] = options.location + '.' + options.connectivity
 
-  if options.tcpdump:
-    test_params['tcpdump'] = options.tcpdump
-  if options.script:
-    test_params['script'] = open(options.script, 'rb').read()
-  if options.key:
-    test_params['k'] = options.key
+    if options.tcpdump:
+        test_params['tcpdump'] = options.tcpdump
+    if options.script:
+        test_params['script'] = open(options.script, 'rb').read()
+    if options.key:
+        test_params['k'] = options.key
 
-  if isinstance(options.urlfile, list) or isinstance(options.urlfile, tuple):
-    requested_urls = options.urlfile
-  else:
-    requested_urls = wpt_batch_lib.ImportUrls(options.urlfile)
-  id_url_dict = wpt_batch_lib.SubmitBatch(requested_urls, test_params,
-                                          options.server)
-  if verbose:
-    print 'Submitted %s urls to be tested' % len(requested_urls)
-    sys.stdout.flush()
+    if isinstance(options.urlfile, list) or isinstance(options.urlfile, tuple):
+        requested_urls = options.urlfile
+    else:
+        requested_urls = wpt_batch_lib.ImportUrls(options.urlfile)
+    id_url_dict = wpt_batch_lib.SubmitBatch(requested_urls, test_params,
+                                            options.server)
+    if verbose:
+        print 'Submitted %s urls to be tested' % len(requested_urls)
+        sys.stdout.flush()
 
-  submitted_urls = set(id_url_dict.values())
-  for url in requested_urls:
-    if url not in submitted_urls:
-      logging.warn('URL submission failed: %s', url)
+    submitted_urls = set(id_url_dict.values())
+    for url in requested_urls:
+        if url not in submitted_urls:
+            logging.warn('URL submission failed: %s', url)
 
-  return id_url_dict
+    return id_url_dict
 
 
 def FinishBatch(id_url_dict, server, outputdir, verbose=False):
-  """Waits for the server to give results for queries from StartBatch.
+    """Waits for the server to give results for queries from StartBatch.
 
-  The main reason to separate these out is that multiple calls to
-  StartBatch can be handled by a single call to FinishBatch (so the
-  work of multiple batches can be done in parallel).  For this to work,
-  though, all batches must uuse the same server and outputdir.
-  """
-  pending_test_ids = id_url_dict.keys()
-  if outputdir and not os.path.isdir(outputdir):
-    os.mkdir(outputdir)
-  retval = {}
-  while pending_test_ids:
-    # TODO(zhaoq): add an expiring mechanism so that if some tests are stuck
-    # too long they will reported as permanent errors and while loop will be
-    # terminated.
+    The main reason to separate these out is that multiple calls to
+    StartBatch can be handled by a single call to FinishBatch (so the
+    work of multiple batches can be done in parallel).  For this to work,
+    though, all batches must uuse the same server and outputdir.
+    """
+    pending_test_ids = id_url_dict.keys()
+    if outputdir and not os.path.isdir(outputdir):
+        os.mkdir(outputdir)
+    retval = {}
+    while pending_test_ids:
+        # TODO(zhaoq): add an expiring mechanism so that if some tests
+        # are stuck too long they will reported as permanent errors
+        # and while loop will be terminated.
 
-    id_status_dict = wpt_batch_lib.CheckBatchStatus(pending_test_ids,
-                                                    server_url=server)
-    completed_test_ids = []
-    for test_id, test_status in id_status_dict.iteritems():
-      # We could get 4 different status codes with different meanings as
-      # as follows:
-      # 1XX: Test in progress
-      # 200: Test complete
-      # 4XX: Test request not found
-      if int(test_status) >= 200:
-        pending_test_ids.remove(test_id)
-        if test_status == '200':
-          completed_test_ids.append(test_id)
-        else:
-          logging.warn('Tests failed with status $s: %s', test_status, test_id)
-    test_results = wpt_batch_lib.GetXMLResult(completed_test_ids,
-                                              server_url=server)
-    result_test_ids = set(test_results.keys())
-    for test_id in completed_test_ids:
-      if test_id not in result_test_ids:
-        logging.warn('The XML failed to retrieve: %s', test_id)
+        id_status_dict = wpt_batch_lib.CheckBatchStatus(pending_test_ids,
+                                                        server_url=server)
+        completed_test_ids = []
+        for test_id, test_status in id_status_dict.iteritems():
+            # We could get 4 different status codes with different meanings
+            # as follows:
+            # 1XX: Test in progress
+            # 200: Test complete
+            # 4XX: Test request not found
+            if int(test_status) >= 200:
+                pending_test_ids.remove(test_id)
+                if test_status == '200':
+                    completed_test_ids.append(test_id)
+                else:
+                    logging.warn('Tests failed with status $s: %s',
+                                 test_status, test_id)
+        test_results = wpt_batch_lib.GetXMLResult(completed_test_ids,
+                                                  server_url=server)
+        result_test_ids = set(test_results.keys())
+        for test_id in completed_test_ids:
+            if test_id not in result_test_ids:
+                logging.warn('The XML failed to retrieve: %s', test_id)
 
-    for test_id, dom in test_results.iteritems():
-      if verbose:
-        print 'Done with %s' % str(id_url_dict[test_id])
-        sys.stdout.flush()
-      retval[id_url_dict[test_id]] = dom
-      if outputdir:
-        SaveTestResult(outputdir, id_url_dict[test_id], test_id,
-                       dom.toxml('utf-8'))
-    if pending_test_ids:
-      time.sleep(10)
+        for test_id, dom in test_results.iteritems():
+            if verbose:
+                print 'Done with %s' % str(id_url_dict[test_id])
+                sys.stdout.flush()
+            retval[id_url_dict[test_id]] = dom
+            if outputdir:
+                SaveTestResult(outputdir, id_url_dict[test_id], test_id,
+                               dom.toxml('utf-8'))
+        if pending_test_ids:
+            time.sleep(10)
 
-  return retval
+    return retval
 
 
 def RunBatch(options, verbose=False):
-  """Run one-off batch processing of WebpageTest testing.
+    """Run one-off batch processing of WebpageTest testing.
 
-  Arguments:
-      options: taken from GetOptions().  However, you may munge them
-        manually to get the following special behavior not possible
-        from just calling this script from the commandline:
-      options.urlfile: if you set this to a list, it will be taken
-        as the list of urls, rather than a filename to read urls
-        from.
-      verbose: print diagnostics as the testing goes along.
+    Arguments:
+        options: taken from GetOptions().  However, you may munge them
+          manually to get the following special behavior not possible
+          from just calling this script from the commandline:
+        options.urlfile: if you set this to a list, it will be taken
+          as the list of urls, rather than a filename to read urls
+          from.
+        verbose: print diagnostics as the testing goes along.
 
-  Returns:
-    A map from url to webpagetest results (as a minidom DOM object).
-  """
-  id_url_dict = StartBatch(options, verbose)
-  return FinishBatch(id_url_dict, options.server, options.outputdir, verbose)
+    Returns:
+      A map from url to webpagetest results (as a minidom DOM object).
+    """
+    id_url_dict = StartBatch(options, verbose)
+    return FinishBatch(id_url_dict, options.server, options.outputdir, verbose)
 
 
 def GetOptions(argv=sys.argv[1:]):
-  """Read options from the commandline and return an options struct."""
-  class PlainHelpFormatter(optparse.IndentedHelpFormatter):
-    def format_description(self, description):
-      if description:
-        return description + '\n'
-      else:
-        return ''
+    """Read options from the commandline and return an options struct."""
+    class PlainHelpFormatter(optparse.IndentedHelpFormatter):
+        def format_description(self, description):
+            if description:
+                return description + '\n'
+            else:
+                return ''
 
-  option_parser = optparse.OptionParser(
-      usage='%prog [options]',
-      formatter=PlainHelpFormatter(),
-      description='')
+    option_parser = optparse.OptionParser(
+        usage='%prog [options]',
+        formatter=PlainHelpFormatter(),
+        description='')
 
-  # Environment settings
-  option_parser.add_option('-s', '--server', action='store',
-                           default='http://your_wpt_site/',
-                           help='the wpt server URL')
-  option_parser.add_option('-i', '--urlfile', action='store',
-                           default='./urls.txt', help='input URL file')
-  option_parser.add_option('-f', '--outputdir', action='store',
-                           default='./result',
-                           help='output directory (blank to suppress output)')
+    # Environment settings
+    option_parser.add_option('-s', '--server', action='store',
+                             default='http://your_wpt_site/',
+                             help='the wpt server URL')
+    option_parser.add_option('-i', '--urlfile', action='store',
+                             default='./urls.txt', help='input URL file')
+    option_parser.add_option('-f', '--outputdir', action='store',
+                             default='./result',
+                             help='output directory ("" to suppress output)')
 
-  # Test parameter settings
-  help_txt = 'set the connectivity to pre-defined type: '
-  help_txt += 'DSL, Dial, Fios and custom (case sensitive). '
-  help_txt += 'When it is custom, you can set the customized connectivity '
-  help_txt += 'using options -u/d/l/p.'
-  option_parser.add_option('-k', '--key', action='store', default='',
-                           help='API Key')
-  option_parser.add_option('-y', '--connectivity', action='store',
-                           default='DSL', help=help_txt)
-  option_parser.add_option('-u', '--bwup', action='store', default=384,
-                           help='upload bandwidth of the test')
-  option_parser.add_option('-d', '--bwdown', action='store', default=1500,
-                           help='download bandwidth of the test')
-  option_parser.add_option('-l', '--latency', action='store', default=50,
-                           help='rtt of the test')
-  option_parser.add_option('-p', '--plr', action='store', default=0,
-                           help='packet loss rate of the test')
-  option_parser.add_option('-v', '--fvonly', action='store', default=1,
-                           help='first view only')
-  option_parser.add_option('-t', '--tcpdump', action='store_true',
-                           help='enable tcpdump')
-  option_parser.add_option('-c', '--script', action='store',
-                           help='hosted script file')
-  option_parser.add_option('-a', '--video', action='store', default=0,
-                           help='capture video')
-  option_parser.add_option('-r', '--runs', action='store', default=9,
-                           help='the number of runs per test')
-  option_parser.add_option('-o', '--location', action='store',
-                           default='Test', help='test location')
-  option_parser.add_option('-m', '--mv', action='store', default=1,
-                           help='video only saved for the median run')
+    # Test parameter settings
+    help_txt = 'set the connectivity to pre-defined type: '
+    help_txt += 'DSL, Dial, Fios and custom (case sensitive). '
+    help_txt += 'When it is custom, you can set the customized connectivity '
+    help_txt += 'using options -u/d/l/p.'
+    option_parser.add_option('-k', '--key', action='store', default='',
+                             help='API Key')
+    option_parser.add_option('-y', '--connectivity', action='store',
+                             default='DSL', help=help_txt)
+    option_parser.add_option('-u', '--bwup', action='store', default=384,
+                             help='upload bandwidth of the test')
+    option_parser.add_option('-d', '--bwdown', action='store', default=1500,
+                             help='download bandwidth of the test')
+    option_parser.add_option('-l', '--latency', action='store', default=50,
+                             help='rtt of the test')
+    option_parser.add_option('-p', '--plr', action='store', default=0,
+                             help='packet loss rate of the test')
+    option_parser.add_option('-v', '--fvonly', action='store', default=1,
+                             help='first view only')
+    option_parser.add_option('-t', '--tcpdump', action='store_true',
+                             help='enable tcpdump')
+    option_parser.add_option('-c', '--script', action='store',
+                             help='hosted script file')
+    option_parser.add_option('-a', '--video', action='store', default=0,
+                             help='capture video')
+    option_parser.add_option('-r', '--runs', action='store', default=9,
+                             help='the number of runs per test')
+    option_parser.add_option('-o', '--location', action='store',
+                             default='Test', help='test location')
+    option_parser.add_option('-m', '--mv', action='store', default=1,
+                             help='video only saved for the median run')
 
-  options, args = option_parser.parse_args(argv)
-  return options
+    options, args = option_parser.parse_args(argv)
+    return options
 
 
 def main():
-  RunBatch(GetOptions())
+    RunBatch(GetOptions())
 
 if __name__ == '__main__':
-  main()
+    main()
