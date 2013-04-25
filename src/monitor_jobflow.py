53de52a88bc82eaa244d676f5f188f20c11de1a4
Eliana Feasley <eliana@khanacademy.org>
2013-04-25 09:58:44 -0700

    Alerting in subject of emails when a step in a job fails
    Summary:
    When a step in a job fails, the hipchat room is alerted, but it still
    says jobflow status = SUCEEDED, which is technically true. This change adds a
    note that says that a step failed under tha same conditions that cause a
    hipchat notification to be sent out, for clarity in hipchat and email subject
    lines.
    
    Test Plan:
    Tested in python shell looking at jobflow that had sucessful status
    but a failed step.
    
    Old subject: Jobflow status = COMPLETED (j-24PGH7FNPSP76: 2013-04-16 GAE Upload)
    
    New subject: Step Failed: Jobflow status = COMPLETED (j-24PGH7FNPSP76: 2013-04-16 GAE Upload)
    
    Reviewers: jace
    
    Differential Revision: http://phabricator.khanacademy.org/D2160

--- src/monitor_jobflow.py
+++ src/monitor_jobflow.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-"""Small wrapper monitors the success or failure of steps for a 
+"""Small wrapper monitors the success or failure of steps for a
 jobflow_id, and sends email about the result.
 """
 
@@ -15,22 +15,24 @@ import notify
 
 def monitor_jobflow(jobflow_id):
     status = emr.wait_for_completion(jobflow_id)
-    
+
     listing = emr.list_steps(jobflow_id)
     jobname = jobflow_id
     heading = listing.split("\n")[0]
-    # there just happens to be a fixed number of characters (85) in the 
+    # there just happens to be a fixed number of characters (85) in the
     # output of the 'elastic-mapreduce --list' command before the jobname
     if len(heading) > 85:
         jobname += ": " + heading[85:]
 
     subject = "Jobflow status = %s (%s)" % (status, jobname)
-    
-    # Until we get more confident, always send email, even on success
-    notify.send_email(subject, listing)
+
     failures = ["FAILED", "CANCELLED", "TERMINATED"]
     if any(s in listing for s in failures):
-        notify.send_hipchat(subject) 
+        subject = "Step Failed: " + subject
+        notify.send_hipchat(subject)
+
+    # Until we get more confident, always send email, even on success
+    notify.send_email(subject, listing)
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
