277259a5bad451d5aadd31f3bfbd75109f6d69ff
Robert Kruszewski <dzobert@gmail.com>
2013-05-10 10:31:31 -0700

    Bunch of changes that would be useful to be tracked. Updates to third_party dependencies
    Summary:
    Update Bootstrap CSS to 2.3.1
    
    Fix navbar for new bootstrap version
    
    Update Backbone to 1.0 version
    
    Update underscore to 1.4.4 version
    
    Update bootstrap dropdown plugin to 2.3.1. Same version as css theme
    
    Add exercise summary to main page of dashboards and dropdown menu
    
    Livereload server configuration for dashboards
    
    To use run:
    pip install livereload
    livereload refresh.py
    
    Image representing exercise summary for dashboards main page
    
    Rather large bunch of external libraries used by exercise summary
    
    Test Plan:
    Go to dashboards.khanacademy.org and verify that dropdowns are sitll working
    
    At dashboards.khanacademy.org verify that exercise summary shows up
    both in dropdown and main list
    
    Run livereload refresh.py, connect browser plugin and verify that page
    is refreshed after every save
    
    Verify that dashboards.khanacademy.org has image under exercise summary.
    
    nothing blows up accidentaly
    
    Reviewers: jace
    
    Reviewed By: jace
    
    Differential Revision: http://phabricator.khanacademy.org/D2228

---
+++ webapps/dashboards/reload.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from livereload.task import Task
+
+Task.add('static/css/*.css')
+Task.add('*.py')
+Task.add('static/css/third_party/*.css')
+Task.add('static/js/third_party/*.js')
+Task.add('static/js/*.js')
+Task.add('templates/*.html')
+Task.add('templates/webpagetest/*.html')
+Task.add('templates/gae-stats/*.html')
+
