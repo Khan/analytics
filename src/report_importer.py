Khan/analytics

commit 7bb6d04bdc36eb894581a9979c849973c1281ce6

Rewriting company_metrics.q for performance (and accuracy?)
Test Plan:
1. Started my own hive cluster, copy+pasted company_metrics.q into hive to execute
2. `select * company_metrics_new;` produces:

2012-04 200455  234261  110996
2012-05 157416  232637  104437
2012-06 153805  169153  83004
2012-07 126050  145249  77356
2012-08 242957  148085  81290
2012-09 519804  215446  162041
2012-10 418853  326151  193460
2012-11 247161  348870  156883
2012-12 199803  329898  132050
2013-01 299278  376685  165444
2013-02 239556  356060  138591
2013-03 240054  369366  140862
2013-04 372975  406869  153169
2013-05 167038  243144  47856

Which feels right in the scheme of our webiste, but are drastically different numbers than what is currently in `select * from company_metrics;`

I have not run the old company_metrics.q script since Jace said it would take forever to complete, so my theories on the differences in output are 1) the company_metrics table is out-of-date, 2) my query is wrong, or 3) the existing query is wrong (due to the `count(distinct user)`?)

So, I need to figure out why there is a discrepancy, but wanted to get a few more eyeballs on this in case I am doing something dumb.

Next Steps:
1. Change the dashboard code to understand the new schema (are there any other callers that would need to be changed?)
2. Run the query over all-time to populate company_metrics

Reviewers: jace

CC: stephanie

Differential Revision: http://phabricator.khanacademy.org/D2329

src/report_importer.py
@@ -82,6 +82,12 @@ def main(table_location,
         # Even though the option is called 'drop', I am instead
         # calling 'remove', because it leaves any indexes while deleting data.
         mongo_collection.remove()
+    else if options.drop_partition:
+        print ("\nDropping existing data in collection {0} "
+                "on partition spec {1}".format(
+                    target_collection, partition_cols)
+
+        mongo_collection.remove(base_partition_values)
 
     # Open our input connections.
     s3conn = boto.connect_s3()
@@ -226,11 +232,16 @@ def parse_command_line_args():
         help=('Number of documents to insert in a single database call. '
               'Will only do bulk inserts if no key index is specified, '
               'because pymongo does not currently support bulk upserts.'))
-    parser.add_option("--drop", action="store_true", 
+    parser.add_option("--drop", action="store_true",
         dest="drop", default=False,
         help=('This flag will delete the entire target collection before '
               'inserting the new data.  Use with caution!'))
-    parser.add_option("--hive_init", action="store_true", 
+    parser.add_option("--drop_partition", action="store_true",
+        dest="drop_partition", default=False,
+        help=('This flag will delete the all rows in the target collection '
+              'that match the value(s) passed in for the partition(s) before '
+              'inserting the new data.  Use with caution!'))
+    parser.add_option("--hive_init", action="store_true",
         dest="hive_init", default=False,
         help=('If True, this script will execute ka_hive_init.q on the '
               'hive_masternode before attempting the import.  This is useful '
@@ -278,16 +289,18 @@ def print_locations(table_location, column_info, partition_cols,
 if __name__ == '__main__':
     start_dt = datetime.datetime.now()
 
+    # Named arguments appear as properties on the options object
+    # Unnamed arguments appears as elements in the args array
     options, args = parse_command_line_args()
 
     # Step 1 - read meta data.
-    hive_masternode = args[0]
+    hive_masternode = args[0]  # Generally, 'ka-hive'
     hive_mysql_connector.configure(hive_masternode, options.ssh_keyfile)
-        
+
     if options.hive_init:
         hive_mysql_connector.run_hive_init()
 
-    table_name = args[1]
+    table_name = args[1]  # Generally == target_collection = args[3]
     print "Fetching table info..."
     table_location = hive_mysql_connector.get_table_location(table_name)
 
@@ -298,9 +311,9 @@ if __name__ == '__main__':
         raise Exception("Can only import from s3://ka-mapreduce for now")
     column_info = hive_mysql_connector.get_table_columns(table_name)
 
-    target_db = args[2]
-    target_collection = args[3]
-    partition_cols = args[4:]
+    target_db = args[2]  # Always 'report'
+    target_collection = args[3]  # Generally == table_name = args[1]
+    partition_cols = args[4:]  # Something like ['dt=2013-05', user='hello']
 
     # TODO(benkomalo): prompt/dry-run flags?
     # Step 2 - print locations

map_reduce/hive/company_metrics.q
@@ -1,66 +1,95 @@
--- This script comptues 3 of the 4 company-wide growth metrics for 2012 Q3.
+-- This script computes 3 of the 4 company-wide growth metrics for
+-- the ranges given by ${start_dt} and ${end_dt}.  This script outputs numbers
+-- on a month-level granularity, so ${start_dt} should always be the first day
+-- of a month.  This script also overwrites the months for which it computes
+-- data, so be sure to always run this script for months that are completed
+-- (in addition to the current month).
+
+-- As of 2013-05-17, this script takes ~25min to populate the company_metrics
+-- table in Hive, and an additional ~1min to push that data from Hive to MongoDB
 
 -- For the definition of the company_metrics table, see ka_hive_init
-
 
 -- Note: Metric 1 (Unique visitors) is easily pulled from Google analytics
 -- (but not from Hive) and thus is not computed here.
 
+-- We must enable dynamic partitions
+set hive.exec.dynamic.partition=true;
+set hive.exec.dynamic.partition.mode=nonstrict;
 
--- Note: userdata_info is a view, and that can apparently cause 
--- problems with "predicate pushdown" query optimization
--- http://karmasphere.com/hive-queries-on-table-data#lateral_view_syntax
-set hive.optimize.ppd=false;
+-- This uses "dynamic partion" writes based on the value of activity_month
+-- NOTE, the order of columns must be the same that comes back with describe company_metrics
+INSERT OVERWRITE TABLE company_metrics PARTITION (activity_month)
+SELECT
+  sum(activity_by_user_month.registered_this_month) as registrations_this_month,
+  sum(activity_by_user_month.long_term) as long_term_users_active_this_month,
+  sum(activity_by_user_month.highly_engaged) as highly_engaged_users_active_this_month,
+  activity_by_user_month.activity_month as activity_month
 
--- Metric 2, Registrations
-INSERT OVERWRITE TABLE company_metrics PARTITION (series='registrations')
-SELECT month, count(distinct user) AS total
 FROM (
-  SELECT 
-    u.user as user, 
-    substr(from_unixtime(floor(u.joined)), 1, 7) AS month
-  FROM userdata_info u
-  WHERE u.registered
-) registrations
-GROUP BY month
-ORDER BY month;
+  SELECT
 
+    activity_by_user_day.activity_month as activity_month,
 
--- Metric 3, Long-term users
-INSERT OVERWRITE TABLE company_metrics PARTITION (series='long term users')
-SELECT month, count(distinct user) AS total
-FROM (
-  SELECT user_daily_activity.user, substr(user_daily_activity.dt, 1, 7) AS month
-  FROM user_daily_activity 
-  JOIN userdata_info
-  ON user_daily_activity.user = userdata_info.user
-  WHERE datediff(user_daily_activity.dt, 
-                 from_unixtime(floor(userdata_info.joined))) >= 28 
-        AND userdata_info.registered
-) jointable
-GROUP BY month
-ORDER BY month;
-  
+    -- Each user only counts towards 1 registration month
+    -- (max is only needed here to avoid "not included in the GROUP BY" errors)
+    CASE WHEN (max(activity_by_user_day.joined_month) = activity_by_user_day.activity_month)
+      THEN 1
+      ELSE 0
+    END as registered_this_month,
 
--- Metric 4, Engagement
-INSERT OVERWRITE TABLE company_metrics PARTITION (series='highly engaged users')
-SELECT month, COUNT(distinct user) AS total
-FROM (
+    -- A user may or may not be counted as a "long term user" for each month.
+    -- Once a user becomes "long term" they will be counted for every month
+    -- that they log at least some activity (because this context is grouped by month)
+    CASE WHEN (
+        -- Pick the activity furthest from registration within this month
+        -- (max is only needed here to avoid "not included in the GROUP BY" errors)
+        datediff(max(activity_by_user_day.activity_dt),
 
-  SELECT 
-    substr(user_daily_activity.dt, 1, 7) AS month, 
-    user_daily_activity.user AS user, 
-    COUNT(1) AS active_visits
-  FROM user_daily_activity
-  JOIN userdata_info
-  ON user_daily_activity.user = userdata_info.user
-  WHERE userdata_info.registered
-  GROUP BY user_daily_activity.user, substr(user_daily_activity.dt, 1, 7)
+        -- And compare that against registration
+        -- (max is only needed here to avoid "not included in the GROUP BY" errors)
+        max(activity_by_user_day.joined_dt))
 
-) monthly
-WHERE monthly.active_visits >= 4
-GROUP BY month
-ORDER BY month;
+        -- If the activity happend more than 28 days from registration, this user is long-term!
+        >= 28)
+      THEN 1
+      ELSE 0
+    END as long_term,
 
--- Reset back to the usual value.  See Comments above.
-set hive.optimize.ppd=true;
+    -- A user may or may not be highly engaged for any given month after their registration
+    CASE WHEN (count(*) >= 4)
+      THEN 1
+      ELSE 0
+    END as highly_engaged
+
+  FROM (
+
+        SELECT
+          userdata_info.user as user,
+          from_unixtime(floor(userdata_info.joined)) as joined_dt,
+          substr(from_unixtime(floor(userdata_info.joined)), 1, 7) as joined_month,
+          user_daily_activity.dt as activity_dt,
+          substr(user_daily_activity.dt, 1, 7) AS activity_month
+
+        FROM user_daily_activity
+        JOIN userdata_info
+          ON user_daily_activity.user = userdata_info.user
+        WHERE
+              -- Note that we don't filter userdata_info.joined. We need to
+              -- pick up all of the users, because they may need to be
+              -- counted as long_term or highly_engaged
+          userdata_info.registered
+          and user_daily_activity.dt >= '${start_dt}'
+          and user_daily_activity.dt < '${end_dt}'
+
+        ) activity_by_user_day
+
+  GROUP BY activity_by_user_day.user, activity_by_user_day.activity_month
+
+) activity_by_user_month
+GROUP BY activity_by_user_month.activity_month
+ORDER BY activity_month;
+
+-- Reset Hive configuration options
+set hive.exec.dynamic.partition=false;
+set hive.exec.dynamic.partition.mode=strict;

map_reduce/hive/ka_hive_init.q
@@ -323,11 +323,12 @@ ALTER TABLE user_growth RECOVER PARTITIONS;
 
 -- Stores monthly time series of registrations, long-term users
 -- and engagement.  Populated in company_metrics.q
-DROP TABLE company_metrics;
+DROP TABLE IF EXISTS company_metrics;
 CREATE EXTERNAL TABLE IF NOT EXISTS company_metrics(
-  month STRING,
-  total INT)
-PARTITIONED BY (series STRING)
+  registrations_this_month INT,
+  long_term_users_active_this_month INT,
+  highly_engaged_users_active_this_month INT)
+PARTITIONED BY (activity_month STRING)
 LOCATION 's3://ka-mapreduce/summary_tables/company_metrics';
 ALTER TABLE company_metrics RECOVER PARTITIONS;
 

map_reduce/load_emr_daily.sh
@@ -16,6 +16,22 @@ day_as_path=$(date --date=${day} '+%Y/%m/%d')
 day_before=$(date --date=${day}-1day '+%Y-%m-%d')
 day_after=$(date --date=${day}+1day '+%Y-%m-%d')
 
+# Calculate the first and last day of the current month for monthly rollup scripts
+# Note that these calculations are based on yesterday, which is done collecting data
+today_day=$(date +%d)
+days_to_subtract=$((${today_day}-1))
+month=$(date --date=${day}-1day '+%Y-%m')
+
+if [ "${days_to_subtract}" = "0" ]; then
+    # If today is the first day of a new month, we want last month
+    month_first_day=$(date --date "-1 month" '+%Y-%m-%d')
+    month_last_day=$(date --date "-1 day" '+%Y-%m-%d')
+else
+    # Else, we can just do the math on today since it works the same as yesterday
+    month_first_day=$(date --date "-${days_to_subtract} days" '+%Y-%m-%d')
+    month_last_day=$(date --date "+1 month -${today_day} days" '+%Y-%m-%d')
+fi
+
 current_dir=`dirname $0`
 archive_dir="$HOME/kabackup/daily_new"
 
@@ -60,7 +76,7 @@ echo "$status"
 jobid=$(echo "$status" | awk '{print $4}')
 ${current_dir}/../src/monitor_jobflow.py $jobid &
 
-# UserData update
+# Update *Incr and *P tables, for example UserDataIncr and UserDataP
 echo "Updating the UserData"
 status=$(elastic-mapreduce --create --name "${day} UserDataP" \
   --num-instances 3 --master-instance-type m1.small \
@@ -87,9 +103,11 @@ echo "$status"
 jobid=$(echo "$status" | awk '{print $4}')
 ${current_dir}/../src/monitor_jobflow.py $jobid &
 
-# Daily reports 
+# Daily reports
 echo "Generating daily reports"
 ${current_dir}/../src/report_generator.py \
   -c ${current_dir}/../cfg/daily_report.json \
-  "<day>=${day}" "<day_before>=${day_before}" "<day_after>=${day_after}" 2>&1
+  "<day>=${day}" "<day_before>=${day_before}" "<day_after>=${day_after}" \
+  "<month_first_day>=${month_first_day}" "<month_last_day>=${month_last_day}" \
+  "<month>=${month}" 2>&1
 

src/emr.py
@@ -102,7 +102,7 @@ def create_hive_cluster(job_name, options, hive_script=None, script_args={}):
     return jobflow_id
 
 
-def add_hive_step(jobflow_id, options, 
+def add_hive_step(jobflow_id, options,
                   hive_script, script_args={},
                   step_name=None):
     """Add a Hive jobflow step to the specififed jobflow.
@@ -121,12 +121,12 @@ def add_hive_step(jobflow_id, options,
     options.update(overrides)
 
     if not step_name:
-        script_base_name = hive_script.split("/")[-1].split(".")[0] 
+        script_base_name = hive_script.split("/")[-1].split(".")[0]
         step_name = script_base_name + ": " + str(script_args)
 
     args = ['elastic-mapreduce',
             '--jobflow', jobflow_id,
-            '--hive-script', 
+            '--hive-script',
             '--step-name', step_name,
             '--arg', hive_script,
             '--args', '-i,"s3://ka-mapreduce/code/hive/ka_hive_init.q"'
@@ -151,8 +151,17 @@ def list_steps(jobflow_id=None):
     return popen_results(args)[0].strip()
 
 
-def wait_for_completion(jobflow_id, sleep=60):
-    """Use polling to wait until a jobflow is complete and report status."""
+def wait_for_completion(jobflow_id, sleep=60, logger=None):
+    """Use polling to wait until a jobflow is complete and report status.
+
+    Arguments:
+        jobflow_id - the Hive job flow id
+        sleep - time in seconds to sleep between status queries
+        logger - a util.get_logger() that implements an .info() method
+
+    Returns:
+        The final job status (Generally 'COMPLETE', 'FAILED' or 'TERMINATED')
+    """
 
     complete = False
     while not complete:
@@ -163,6 +172,9 @@ def wait_for_completion(jobflow_id, sleep=60):
                     % jobflow_id)
         job_info = listing.split("\n")[0]
         job_status = job_info.split()[1]
+
+        if logger:
+            logger.info(job_status)
 
         if job_status not in ['STARTING', 'RUNNING', 'SHUTTING_DOWN']:
             # job complete, probably with the status

src/report_generator.py
@@ -159,7 +159,7 @@ def run_hive_jobs(jobname, steps, num_instances):
                           hive_script=step["hive_script"],
                           script_args=step["hive_args"])
 
-    status = emr.wait_for_completion(jobflow)
+    status = emr.wait_for_completion(jobflow, logger=g_logger)
     listing = emr.list_steps(jobflow)
     failures = ["FAILED", "CANCELLED", "TERMINATED"]
     if any(s in listing for s in failures):
@@ -189,11 +189,21 @@ def run_report_importer(hive_masternode, steps):
             options += ' --drop'
         if step.get('hive_init', False):
             options += ' --hive_init'
-        #TODO(benkomalo): Make the report_importer callable
+
+        # TODO(benkomalo): Make the report_importer callable
+        # An example of the arg-string composition below is something like:
+        # --drop --hive_init ka-hive my_tbl report my_tbl dt=2013-05 name=hello
+        # %s = --drop --hive_init (named options parsed by report_importer.py)
+        # %s = ka-hive (args[0] in report_importer.py)
+        # %s = my_tbl
+        # report (which is the hard-coded mongodb)
+        # %s = my_tbl
+        # %s = dt='2013-05' name='hello'
         command = ('python /home/analytics/analytics/src/report_importer.py'
                    ' %s %s %s report %s %s') % (
                    options, hive_masternode, step['hive_table'],
                    step['mongo_collection'], step['importer_args'])
+
         g_logger.info("Running command: \n%s" % (command))
         proc = subprocess.Popen(command.split(),
             stdout=subprocess.PIPE, stderr=subprocess.PIPE)

webapps/README
@@ -1,2 +1,11 @@
 This folder contains subfolders, each of which should be a fully
-self-contained and deployable web app (such as a dashboard)
+self-contained and deployable web app (such as a dashboards).
+
+To start the dashboards web server run this command to start the webserver on port 5000
+    analytics/webapps/dashboards/main.py -d
+
+TIPS:
+- You may want to create a seperate virtualenv for the analytics repo
+- Make sure you have run `pip install -r requirements.txt` from the root of the analytics repo
+--- If this doesn't work, you can try installing items one-at-time until it works
+- If you get an error about a Fortran installer, try this page:  http://stackoverflow.com/questions/11442970/numpy-and-scipy-for-preinstalled-python-2-6-7-on-mac-os-lion

webapps/dashboards/static/js/company-goals.js
@@ -76,18 +76,33 @@ var fetchGrowthData = function() {
  */
 var handleGrowthData = function(data) {
 
-    series = {};
+    series = {
+        "registrations": {
+            "name": "registrations",
+            "data": []
+        },
+        "long term users": {
+            "name": "long term users",
+            "data": []
+        },
+        "highly engaged users": {
+            "name": "highly engaged users",
+            "data": []
+        }
+    };
+
+    // Sort based on month
+    data = _.sortBy(data, "activity_month");
 
     _.each(data, function(record) {
-        var curr_series = record["series"];
-        if (!(curr_series in series)) {
-            series[curr_series] = {
-                "name": curr_series,
-                "data": []
-            };
-        }
-        series[curr_series]["data"].push(
-            [monthStringToDate(record["month"]), record["total"]]
+        series["registrations"]["data"].push(
+            [monthStringToDate(record["activity_month"]), record["registrations_this_month"]]
+        );
+        series["long term users"]["data"].push(
+            [monthStringToDate(record["activity_month"]), record["long_term_users_active_this_month"]]
+        );
+        series["highly engaged users"]["data"].push(
+            [monthStringToDate(record["activity_month"]), record["highly_engaged_users_active_this_month"]]
         );
     });
 

commit fc81ddf30aa326c8de92e7c8a996a7ee5ffadd8c

Fix syntax error introduced by D2329 fix report_importer syntax errors
Summary: wrong else if statement and missing parenthesis

Test Plan: report_importer.py should not have syntax errors

Reviewers: mattfaus, jace

Reviewed By: mattfaus

Differential Revision: http://phabricator.khanacademy.org/D2367

src/report_importer.py
@@ -82,10 +82,10 @@ def main(table_location,
         # Even though the option is called 'drop', I am instead
         # calling 'remove', because it leaves any indexes while deleting data.
         mongo_collection.remove()
-    else if options.drop_partition:
+    elif options.drop_partition:
         print ("\nDropping existing data in collection {0} "
                 "on partition spec {1}".format(
-                    target_collection, partition_cols)
+                    target_collection, partition_cols))
 
         mongo_collection.remove(base_partition_values)
 