26916b79eeed4ecaa7c7a72e8f402c476f47d52a
sckh <sitanchen@khanacademy.org>
2013-07-15 18:19:52 -0700

    Super-simple reducer script for parsing kalog, to be used in annotating the user_experiment_info hive table with growth mindset header messages
    Test Plan: Played around with sample kalogs in python shell to confirm I didn't
    screw up the indices. ka-hive is taking forever so I'll commit the changes to scripts_combined.q tomorrow after I've tested.

---
+++ map_reduce/py/kalog_parser.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+"""Quick reducer script to parse each kalog and, if it's associated with 
+the growth mindset experiment, returns the message text of the header.
+
+Input:
+	Tab-delimited rows of [bingo_id, kalog]
+
+Output:
+	For each log, emits the value in the x.message_text field, if it exists
+"""
+
+def main():
+	key1 = 'x.mindset.message_text'
+	key2 = 'id.bingo'
+	for line in sys.stdin:
+		bingo_id, kalog = line.rstrip('\n').split('\t')
+		start = kalog.find(key1)
+		end = kalog.find(key2)
+		if start >= 0 and end >= 0:
+			start += len(key1) + 1
+			end -= 1
+		message_text = kalog[start:end]
+		print '\t'.join([bingo_id, message_text])
+
+if __name__ == '__main__':
+	main()
