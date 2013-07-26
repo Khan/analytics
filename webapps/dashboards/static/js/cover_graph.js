4cbb3b90f9a922720c0bfb1f320938516e9f2f0a
sckh <sitanchen@khanacademy.org>
2013-07-25 19:00:00 -0700

    New cover graph layout using dagre (DAG renderer) library, search is more visual, re-renders the connected components of all the points which fall under the search query
    Test Plan: Looks fine locally

--- webapps/dashboards/static/js/cover_graph.js
+++ webapps/dashboards/static/js/cover_graph.js
@@ -5,64 +5,42 @@ var w = 1000,
     timeout = null,
     node = null,
     path = null,
-    oldMatches = null;
-
+    oldMatches = null,
+    nodePadding = 10;
 
 d3.json('/static/cover_graph.json', function(json) {
-  var vis = d3.select("#chart")
-      .append("svg:svg")
-        .attr("width", "100%")
-        .attr("height", "100%")
-        .on("click", function() {if(node && path) clear(1);})
-      .append('svg:g')
-        .call(d3.behavior.zoom().scaleExtent([0.25,8]).on("zoom", redraw))
-      .append('svg:g');
 
-  vis.append('svg:rect')
-    .attr("width", w)
-    .attr("height", h)
-    .attr("fill", "white")
-    .style("pointer-events", 'all')
-
-  vis.append("svg:defs").selectAll("marker")
-      .data(["end"])
-    .enter().append("svg:marker")
-      .attr("id", String)
-      .attr("viewBox", "0 -5 10 10")
-      .attr("refX", 20)
-      .attr("refY", -2)
-      .attr("markerWidth", 9)
-      .attr("markerHeight", 9)
-      .attr("orient", "auto")
-    .append("svg:path")
-      .attr("d", "M0,-5L10,0L0,5");
-
-  function redraw() {
-    vis.attr("transform",
-        "translate(" + d3.event.translate + ")"
-        + " scale(" + d3.event.scale + ")");
-  }
-
-  var force = d3.layout.force()
-      .nodes(json.nodes)
-      .links(json.links)
-      .size([w,h])
-      .charge([-2000])
-      .gravity(0.5)
-      .on("tick", tick)
-      .start();
+  json.nodes.forEach(function (d) {
+    d.edges = [];
+  })
+  json.links.forEach(function (e) {
+    json.nodes[e.source].edges.push(e);
+    json.nodes[e.target].edges.push(e);
+  });
+  json.links.forEach(function (e) {
+    e.source = json.nodes[e.source];
+    e.target = json.nodes[e.target];
+  })
 
   // Adjacency matrix
   var adjMatrix = {};
   json.links.forEach(function (d) {
-    adjMatrix[d.source.index + "," + d.target.index] = 1
+    adjMatrix[d.source.id + "," + d.target.id] = 1
   });
+
+  // helper function for checking whether v1 -> v2 is an edge
+  function isEdge(v1, v2) {
+    if(adjMatrix[v1.id + "," + v2.id])
+      return true;
+    else
+      return false;
+  }
 
   // returns list of id's of nodes covering v1
   function getCovers(v1) {
-    covers = new Array();
+    var covers = new Array();
     json.nodes.forEach(function (v2) {
-      if(adjMatrix[v2.index + "," + v1.index])
+      if(isEdge(v2,v1))
         covers.push(v2);
     })
     return covers;
@@ -70,9 +48,9 @@ d3.json('/static/cover_graph.json', function(json) {
 
   // returns list of id's of nodes covered by v1
   function getCovered(v1) {
-    covered = new Array();
+    var covered = new Array();
     json.nodes.forEach(function (v2) {
-      if(adjMatrix[v1.index + "," + v2.index])
+      if(isEdge(v1,v2))
         covered.push(v2);
     })
     return covered;
@@ -81,7 +59,7 @@ d3.json('/static/cover_graph.json', function(json) {
   // check if node is connected to any node in arr
   function connected(v1, arr) {
     return arr.some( function(v2) {
-      return adjMatrix[v1.index + "," + v2.index] || adjMatrix[v2.index + "," + v1.index] || v1.index == v2.index;
+      return (isEdge(v1,v2)|| isEdge(v2,v1) || v1.id == v2.id);
     });
   }
 
@@ -92,63 +70,318 @@ d3.json('/static/cover_graph.json', function(json) {
     });
   }
 
-  var tooltip = d3.select("body").append("div")   
-    .attr("class", "tooltip")               
-    .style("opacity", 0)
+  function getAdjacent(v) {
+    var adjacentEdges = new Array();
+    json.links.forEach(function (e) {
+      if(adjMatrix[v.id + ',' + e.source.id] || adjMatrix[v.id + ',' + e.source.id])
+        adjacentEdges.push(e);
+    })
+    return adjacentEdges;
+  }
+
+  function getChildren(v) {
+    var children = [];
+    var edges = []
+    function getChildrenHelper(v1) {
+      children.push(v1)
+      json.nodes.forEach(function (v2) {
+        if(isEdge(v1, v2)){
+          var result = json.links.filter(function (d) {
+            return (d.source.id == v1.id && d.target.id == v2.id);
+          })[0];
+          edges.push(result);
+          getChildrenHelper(v2);
+        }
+      })
+    }
+    getChildrenHelper(v);
+    return [children, edges];
+  }
+
+  function getParents(v) {
+    var parents = [];
+    var edges = []
+    function getParentsHelper(v1) {
+      parents.push(v1)
+      json.nodes.forEach(function (v2) {
+        if(isEdge(v2, v1)){
+          var result = json.links.filter(function (d) {
+            return (d.source.id == v2.id && d.target.id == v1.id);
+          })[0];
+          edges.push(result);
+          getParentsHelper(v2);
+        }
+      })
+    }
+    getParentsHelper(v);
+    return [parents, edges];
+  }
+
+  function getConnectedComponent(v) {
+    nodes = getChildren(v)[0].concat(getParents(v)[0]);
+    edges = getChildren(v)[1].concat(getParents(v)[1]);
+    return [nodes, edges];
+  }
+
+  displayConnectedComponent(json.nodes, json.links)
+
+  $('#divs').keyup(function(event) {
+    $('#ex-info').empty();
+    val = $('#divs').val();
+    if(val != ""){
+      matches = json.nodes
+        .filter(function (d) {
+          return ((d.id).indexOf(val) == 0);
+      })
+      var tmp_nodes = [];
+      var tmp_links = [];
+
+      matches.forEach(function (d) {
+        tmp_nodes = tmp_nodes.concat(getConnectedComponent(d)[0]);
+        tmp_links = tmp_links.concat(getConnectedComponent(d)[1]);
+      })
+
+      var nodes = []
+      nodes = tmp_nodes.filter(function (d, i) {
+        return tmp_nodes.indexOf(d) == i;
+      })
+      tmp_links = tmp_links.filter(function (d, i) {
+        return tmp_links.indexOf(d) == i;
+      })
+
+      var results = displayConnectedComponent(nodes, tmp_links)
+
+      // matches.forEach(function (d) {
+      //   infoString = '<div class="card"><div class="card-title" id="card-"' + d.id + '">' +d.id + '</div>'
+      //   var covers = getCovers(d);
+      //   var covered = getCovered(d);
+      //   if(covers.length != 0){
+      //     infoString += '<div class="cover-list">Covered by: '
+      //     var max = covers.length;
+      //     var counter = 0;
+      //     covers.forEach(function (d) {
+      //       counter += 1;
+      //       infoString += d.id;
+      //       if(counter != max)
+      //         infoString += ', ';
+      //     })
+      //     infoString += '</div>';
+      //   }
+      //   if(covered.length != 0){
+      //     infoString += '<div class="cover-list">Covers: '
+      //     covered.forEach(function (d) {
+      //       infoString += d.id + ', ';
+      //     })
+      //     infoString += '</div>';
+      //   }
+      //   infoString += '</div>';      
+      //   $('#ex-info').append(infoString);
+      //   $('.card-title').data("node", d)
+      // })
+      
+      path = results[0];
+      node = results[1];
+
+      console.log(node)
+      node
+        .each(function(d) {
+            opacity = connected(d, matches) ? 1 : 0.3;
+            stroke = (matches.indexOf(d) > -1) ? 5 : 1;
+            d3.select(this)
+              .style("opacity", opacity)
+              .style("stroke-width", stroke)
+        })
+      path
+        .each(function(d) {
+            opacity = adjacent(d, matches) ? 1 : 0.3;
+            d3.select(this)
+              .style("opacity", opacity)
+        })
+    }
+    else
+      displayConnectedComponent(json.nodes, json.links)
+  });
+
+});
+
+
+function displayConnectedComponent(nodes, links) {
+  function spline(e) {
+    var points = e.dagre.points.slice(0);
+    var source = dagre.util.intersectRect(e.source.dagre, points[0]);
+    var target = dagre.util.intersectRect(e.target.dagre, points[points.length - 1]);
+    points.unshift(source);
+    points.push(target);
+    return d3.svg.line()
+      .x(function(d) { return d.x; })
+      .y(function(d) { return d.y; })
+      .interpolate("linear")
+      (points);
+  }
+
+  // Translates all points in the edge using `dx` and `dy`.
+  function translateEdge(e, dx, dy) {
+    e.dagre.points.forEach(function(p) {
+      p.x = Math.max(0, Math.min(svgBBox.width, p.x + dx));
+      p.y = Math.max(0, Math.min(svgBBox.height, p.y + dy));
+    });
+  }
+
+  function redraw() {
+    vis.attr("transform",
+        "translate(" + d3.event.translate + ")"
+        + " scale(" + d3.event.scale + ")");
+  }
+
+  // empty the canvas
+  $("#chart").empty()
+
+  var vis = d3.select("#chart")
+      .append("svg:svg")
+        .on("click", function() {if(node && path) clear(1);})
+        .call(d3.behavior.zoom().on("zoom", redraw))
+      .append('svg:g')
+
+  vis.append("svg:defs").selectAll("marker")
+      .data(["end"])
+    .enter().append("svg:marker")
+      .attr("id", "end")
+      .attr("viewBox", "0 0 10 10")
+      .attr("refX", 8)
+      .attr("refY", 5)
+      .attr("markerWidth", 8)
+      .attr("markerHeight", 5)
+      .attr("orient", "auto")
+    .append("svg:path")
+      .attr("d", "M 0 0 L 10 5 L 0 10 z");
 
   var path = vis.selectAll("path.link")
-    .data(json.links)
+    .data(links)
   .enter().append("svg:path")
     .attr("class", "link")
     .attr("marker-end", "url(#end)")
-    .attr("data", function(d) { return "source:"+" target:"+d.target })
 
-  var node = vis.selectAll("circle.node")
-    .data(json.nodes)
-  .enter().append("svg:circle")
-    .attr("class", "node")
-    .attr("id", function(d) { return d.id; })
-    .attr("cx", function(d) { return d.x; })
-    .attr("cy", function(d) { return d.y; })
-    .attr("r", 15)
-    .style("fill", function(d) { return fill(d.id); })
-    .on("mouseover", function(d) {
-      tooltip
-        .style("opacity", 1)
-        .text(d.id)
-      val = $('#divs').val();
-      if(val == "") {
-        d3.select(this)
-          .transition()
-          .duration(400)
-          .attr("r", 30)
-      }
-    })
-    .on("mousemove", function(){
-      return tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");
-    })
+  var node = vis.selectAll("g.node")
+    .data(nodes)
+    .enter()
+      .append("g")
+      .attr("class", "node")
+      .attr("id", function(d) { return d.id; })
 
-    .on("click", function(d) {
-      specialZoom(d, 3);
-    })
+    // .on("mouseover", function(d) {
+    //   tooltip
+    //     .style("opacity", 1)
+    //     .text(d.id)
+    //   val = $('#divs').val();
+    //   if(val == "") {
+    //     d3.select(this)
+    //       .transition()
+    //       .duration(400)
+    //       .attr("r", 30)
+    //   }
+    // })
     .on("mouseleave", function(d) { 
       val = $('#divs').val();
       if(val == "")
         clear(1);
     })
-    .on("mouseout", function(){return tooltip.style("opacity", 0);})
-    .call(force.drag);
+    // .on("mouseout", function(){return tooltip.style("opacity", 0);})
 
-  vis.style("opacity", 1e-6)
-  .transition()
-    .duration(1000)
-    .style("opacity", 1);
+  var rects = node
+    .append("rect")
 
-  node.append("text")
-      .attr("x", 12)
-      .attr("dy", ".35em")
-      .text(function(d) { return d.source; });
+  var labels = node
+    .append("text")
+      .attr("text-anchor", "middle")
+      .attr("x", 0);
 
+  labels
+    .append("tspan")
+    .attr("x", 0)
+    .attr("dy", "1em")
+    .text(function(d) { return d.id; });
+
+  labels.each(function(d) {
+    var bbox = this.getBBox();
+    d.bbox = bbox;
+    d.width = bbox.width + 2 * nodePadding;
+    d.height = bbox.height + 2 * nodePadding;
+  });
+
+  rects
+    .attr("x", function(d) { return -(d.bbox.width / 2 + nodePadding); })
+    .attr("y", function(d) { return -(d.bbox.height / 2 + nodePadding); })
+    .attr("width", function(d) { return d.width; })
+    .attr("height", function(d) { return d.height; })
+
+  labels
+    .attr("x", function(d) { return -d.bbox.width / 2; })
+    .attr("y", function(d) { return -d.bbox.height / 2; });
+
+  dagre.layout()
+    .nodeSep(50)
+    .edgeSep(10)
+    .rankSep(50)
+    .nodes(nodes)
+    .edges(links)
+    .debugLevel(1)
+    .run();
+
+  node.attr("transform", function(d) { return 'translate('+ d.dagre.x +','+ d.dagre.y +')'; });
+
+  path.each(function(d) {
+    var points = d.dagre.points;
+    if (!points.length) {
+      var s = d.source.dagre;
+      var t = d.target.dagre;
+      points.push({ x: (s.x + t.x) / 2, y: (s.y + t.y) / 2 });
+    }
+
+    if (points.length === 1) {
+      points.push({ x: points[0].x, y: points[0].y });
+    }
+  });
+
+  path
+    // Set the id. of the SVG element to have access to it later
+    .attr('id', function(e) { return e.dagre.id; })
+    .attr("d", function(e) { return spline(e); });
+
+  var svgBBox = vis.node().getBBox();
+  vis.attr("width", svgBBox.width + 10);
+  vis.attr("height", svgBBox.height + 10);
+
+
+  var nodeDrag = d3.behavior.drag()
+    // Set the right origin (based on the Dagre layout or the current position)
+    .origin(function(d) { return d.pos ? {x: d.pos.x, y: d.pos.y} : {x: d.dagre.x, y: d.dagre.y}; })
+    .on('drag', function (d, i) {
+      var prevX = d.dagre.x,
+          prevY = d.dagre.y;
+
+      // The node must be inside the SVG area
+      d.dagre.x = Math.max(d.width / 2, Math.min(svgBBox.width - d.width / 2, d3.event.x));
+      d.dagre.y = Math.max(d.height / 2, Math.min(svgBBox.height - d.height / 2, d3.event.y));
+      d3.select(this).attr('transform', 'translate('+ d.dagre.x +','+ d.dagre.y +')');
+
+      var dx = d.dagre.x - prevX,
+          dy = d.dagre.y - prevY;
+
+      // Edges position (inside SVG area)
+      d.edges.forEach(function(e) {
+        translateEdge(e, dx, dy);
+        d3.select('#'+ e.dagre.id).attr('d', spline(e));
+      });
+    });
+
+  var edgeDrag = d3.behavior.drag()
+    .on('drag', function (d, i) {
+      translateEdge(d, d3.event.dx, d3.event.dy);
+      d3.select(this).attr('d', spline(d));
+    });
+
+  node.call(nodeDrag);
+  path.call(edgeDrag);
 
   function tick() {
     path.attr("d", function(d) {
@@ -172,23 +405,10 @@ d3.json('/static/cover_graph.json', function(json) {
       .transition()
       .duration(200)
       .style("opacity", opacity)
-      .attr("r", 15)
     path
       .transition()
       .duration(200)
       .style("opacity", opacity)
-  }
-
-  var zoom = d3.behavior.zoom();
-
-  function specialZoom(d, factor) {
-    x = vis.selectAll("#" + d.id);
-    var transx = (-parseInt(x.attr("cx")) * factor + w / 2),
-        transy = (-parseInt(x.attr("cy")) * factor + h / 2);
-    vis.transition()
-      .attr("transform", "translate(" + transx + "," + transy + ")scale(" + factor + ")");
-    zoom.scale(factor);
-    zoom.translate([transx, transy]);
   }
 
   function show(matches, background, foreground) {
@@ -205,7 +425,7 @@ d3.json('/static/cover_graph.json', function(json) {
       }).attr("r", 15)
       matches.filter(function(d) {
           return jQuery.inArray(d, oldMatches) == -1;
-      }).attr("r", 30)
+      }).attr("height", 30)
     }
     else
       matches.attr("r", 30)         
@@ -229,13 +449,18 @@ d3.json('/static/cover_graph.json', function(json) {
       })
 
     matchesArray.forEach(function (d) {
-      infoString = '<div class="card"><div class="card-title" id="card-"' + d.id + '"><h5>' + d.id + '</h5></div>'
+      infoString = '<div class="card"><div class="card-title" id="card-"' + d.id + '">' +d.id + '</div>'
       var covers = getCovers(d);
       var covered = getCovered(d);
       if(covers.length != 0){
         infoString += '<div class="cover-list">Covered by: '
+        var max = covers.length;
+        var counter = 0;
         covers.forEach(function (d) {
-          infoString += d.id + ', ';
+          counter += 1;
+          infoString += d.id;
+          if(counter != max)
+            infoString += ', ';
         })
         infoString += '</div>';
       }
@@ -251,23 +476,6 @@ d3.json('/static/cover_graph.json', function(json) {
       $('.card-title').data("node", d)
     })
   }
+  return [path, node]
+}
 
-  $('#divs').keyup(function(event) {
-    $('#ex-info').empty();
-    val = $('#divs').val();
-    if(val != ""){
-      matches = node
-        .filter(function (d) {
-          return ((d.id).indexOf(val) == 0);
-      })
-      show(matches, 0.1, 1);
-    }
-    else
-      clear(1);
-  });
-
-  $('.card-title').on("click", function() {
-    alert("asdf");
-  })
-
-});
