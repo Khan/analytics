1e39aa7faf57f5a30c7094c5040eb7e13420adf2
Jace Kohlmeier <jace@khanacademy.org>
2013-09-11 13:31:10 -0700

    add a cron-able script do archive the local pipeline data to S3
    Auditors: mattfaus

---
+++ src/archive_to_s3.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+"""Utility script to archive local data to S3.
+
+This is quick-n-dirty.  You can add configuration if you want, but
+for right now it scans relevant directories for data that is older than
+3 months.
+"""
+
+import datetime
+import os
+import subprocess
+
+# How old the data needs to be before we archive it
+STALE_THRESHOLD = datetime.timedelta(weeks=15)
+
+
+def run_shell_command(command_string, dry_run=False):
+    print command_string
+    if not dry_run:
+        command_output = subprocess.check_output(command_string, shell=True)
+        print command_output
+
+
+def archive_daily_new(date_string):
+    local_path = '/home/analytics/kabackup/daily_new/%s/' % date_string
+    s3_path = 's3://ka-mapreduce/rawdata/%s/' % date_string
+
+    if not os.path.exists(local_path):
+        return
+
+    cmd = 's3cmd sync %s %s' % (local_path, s3_path)
+    run_shell_command(cmd)
+
+    cmd = 'rm -rf %s' % (local_path,)
+    run_shell_command(cmd)
+
+
+def main():
+    today = datetime.datetime.now()
+    archive_end = today - STALE_THRESHOLD
+    archive_start = archive_end - datetime.timedelta(weeks=52)
+
+    date = archive_start
+    while date < archive_end:
+        date_string = date.strftime('%Y-%m-%d')
+        archive_daily_new(date_string)
+        date += datetime.timedelta(days=1)
+
+
+if __name__ == '__main__':
+    main()
