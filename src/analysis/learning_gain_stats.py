e8423848b1a940402f58695c765fcbf4877f747a
Tony Liu <tony@khanacademy.org>
2014-01-09 11:52:52 -0800

    Add efficiency graph by task type
    Test Plan:

--- src/analysis/learning_gain_stats.py
+++ src/analysis/learning_gain_stats.py
@@ -19,6 +19,7 @@ import time
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 def read_data(filename=None):
     if filename is None:
         f = sys.stdin
@@ -27,10 +28,48 @@ def read_data(filename=None):
 
     data = []
     for row in f:
+        # skip empty rows '[]\n'
+        if len(row) <= 3:
+            continue
         problems = ast.literal_eval(row)
         data.append(problems)
 
     return data
+
+
+def graph_efficiency(n, data):
+    correct_by_type = {}
+    total_by_type = {}
+    for problems in data:
+        for i in range(min(n, len(problems))):
+            # labels don't like unicode
+            task_type = str(problems[i][0])
+            if task_type not in correct_by_type:
+                correct_by_type[task_type] = np.zeros(n)
+                total_by_type[task_type] = np.zeros(n)
+            if problems[i][1]:
+                correct_by_type[task_type][i] += 1
+            total_by_type[task_type][i] += 1
+
+    plt.title('Efficiency Curve: By Task Type')
+    plt.xlabel('Problem Number')
+    plt.ylabel('Percent Correct')
+
+    for task_type in sorted(correct_by_type):
+        if task_type == 'None':
+            continue
+        correct = correct_by_type[task_type]
+        total = total_by_type[task_type]
+        eff = np.zeros(n)
+        for i in range(n):
+            if total[i] > 0:
+                eff[i] = 1.0 * correct[i] / total[i]
+            else:
+                eff[i] = 0.0
+        plt.plot(eff, label=task_type)
+    plt.legend(loc='lower center', ncol=2)
+    plt.show()
+
 
 def graph_task_type(n, data):
     map_by_type = {}
@@ -44,7 +83,7 @@ def graph_task_type(n, data):
 
     x = []
     label = []
-    for task_type in map_by_type:
+    for task_type in sorted(map_by_type):
         x.append(map_by_type[task_type])
         label.append(task_type)
 
@@ -54,6 +93,7 @@ def graph_task_type(n, data):
     plt.hist(x, n, normed=0, histtype='bar', stacked=True, label=label)
     plt.legend()
     plt.show()
+
 
 def graph_engagement(n, data):
     eng = np.zeros(n)
@@ -65,17 +105,23 @@ def graph_engagement(n, data):
     plt.plot(eng)
     plt.show()
 
+
 def main():
     start = time.time()
     n = 100
     data = read_data()
     print 'Done reading input, elapsed: %f\n' % (time.time() - start)
 
+    graph_efficiency(n, data)
+    print 'Done graphing efficiency, elapsed: %f\n' % (time.time() - start)
+
+    """
     graph_task_type(n, data)
     print 'Done graphing task type, elapsed: %f\n' % (time.time() - start)
 
     graph_engagement(n, data)
     print 'Done graphing engagement, elapsed: %f\n' % (time.time() - start)
+    """
 
 if __name__ == '__main__':
     main()
