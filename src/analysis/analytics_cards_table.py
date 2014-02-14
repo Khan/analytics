2c001bb96acee2c896423d65586c07fc8aba36b8
Tony Liu <tony@khanacademy.org>
2014-02-14 15:23:08 -0800

    Finish conditional prob calculations
    Test Plan:
    WIP

--- src/analysis/analytics_cards_table.py
+++ src/analysis/analytics_cards_table.py
@@ -44,33 +44,76 @@ def read_data(filename):
                 prev_user_id = user_id
                 data.append([])
             data[-1].append((exercise, correct))
+
     return data
 
 
-def get_exercises(data):
+def get_exercises(data, min_problems):
     exercise_cnt = {}
     for row in data:
-        for exercise, correct in row:
-            if exercise not in exercise_cnt:
-                exercise_cnt[exercise] = 1
+        for e, c in row:
+            if e not in exercise_cnt:
+                exercise_cnt[e] = 1
             else:
-                exercise_cnt[exercise] += 1
-    return exercise_cnt
+                exercise_cnt[e] += 1
+
+    # filter out the retired/experimental exercises
+    exercises = []
+    for e in exercise_cnt:
+        cnt = exercise_cnt[e]
+        if cnt >= min_problems:
+            exercises.append(e)
+    exercises.sort()
+
+    return exercises
 
 
 def compute_and_write(data, min_problems, filename):
     """ Write out the results. Each row is given by:
 
-    exercise,prob_0,prob_1.
+    exercise,prob0,prob1.
 
-    Here, prob_k is the average probability of getting analytics card right,
+    Here, probk is the average probability of getting analytics card right,
     given that we got correct == k on the current exercise.
     """
-    exercises = get_exercises(data)
+    exercises = get_exercises(data, min_problems)
+    exercise_to_index = {}
+    for i, e in enumerate(exercises):
+        exercise_to_index[e] = i
+
     n = len(exercises)
     print 'Num exercises:', n
-    p = np.zeros((n, n))
-    return p
+
+    # pk[i][0][j] / pk[i][1][j] is Pr(X_j = 1 | X_i = k)
+    # the two middle indicies 0 and 1 represent the number correct and total
+    p0 = np.zeros((n, 2, n))
+    p1 = np.zeros((n, 2, n))
+
+    for row in data:
+        prev_e, prev_c = None, False
+        for e, c in row:
+            # skip exercises that were filtered out
+            if e not in exercise_to_index:
+                continue
+
+            # look at our current pair!
+            if prev_e:
+                i = exercise_to_index[prev_e]
+                j = exercise_to_index[e]
+                if prev_c:
+                    p1[i][0][j] += c
+                    p1[i][1][j] += 1
+                else:
+                    p0[i][0][j] += c
+                    p0[i][1][j] += 1
+            prev_e, prev_c = e, c
+
+    # TODO(tony): plot overall exercise accuracy by these values?
+    with open(filename, 'w') as f:
+        for i, e in enumerate(exercises):
+            prob0 = np.mean(p0[i][0] / p0[i][1])
+            prob1 = np.mean(p1[i][0] / p1[i][1])
+            f.write('%s,%.5f,%.5f\n' % (e, prob0, prob1))
 
 
 def main():
@@ -80,7 +123,7 @@ def main():
         help='input file (default is stdin)')
     parser.add_argument('-m', '--min-problems',
         help='minimum number of samples for filtering exercises',
-        type=int, default=10000)
+        type=int, default=1000)
 
     args = parser.parse_args()
 
