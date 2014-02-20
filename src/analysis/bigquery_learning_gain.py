417f294f8e01466c16f29272cd61ae823702e399
Tony Liu <tony@khanacademy.org>
2014-02-20 11:52:53 -0800

    Refactor; simple query test
    Test Plan:
    WIP

--- src/analysis/bigquery_learning_gain.py
+++ src/analysis/bigquery_learning_gain.py
@@ -2,6 +2,14 @@
 Using BigQuery to do a quick-n-dirty learning gain approximation.
 
 This also serves as an experiment in using the BigQuery Python API.
+
+Setup notes:
+- install the python client:
+    pip install google-api-python-client
+- put the project ID in a file, say "id.txt"
+- download the "clients_secrets.json" file for the khan project
+- run!
+    python bigquery_learning_gain.py
 """
 
 import httplib2
@@ -48,17 +56,33 @@ def get_bigquery_service():
     return bigquery_service
 
 
+def run_query(query_string, destination_table, allow_large_results=True):
+    pass
+
+
 def main():
     bigquery_service = get_bigquery_service()
+    project_id = get_project_id()
 
     try:
-        datasets = bigquery_service.datasets()
-        project_id = get_project_id()
-        list_reply = datasets.list(projectId=project_id).execute()
-        print 'Dataset list:'
-        pprint.pprint(list_reply)
+        query_request = bigquery_service.jobs()
+        query_data = {'query': (
+                'SELECT TOP( title, 10) as title, COUNT(*) as revision_count'
+                ' FROM [publicdata:samples.wikipedia] WHERE wp_namespace = 0;'
+            )
+        }
+        query_response = query_request.query(projectId=project_id,
+                                             body=query_data).execute()
+        print 'Query Results:'
+        for row in query_response['rows']:
+            result_row = []
+            for field in row['f']:
+                result_row.append(field['v'])
+            print ('\t').join(result_row)
+
     except HttpError as err:
-        print 'Error in listDatasets:', pprint.pprint(err.content)
+        print 'Error:', pprint.pprint(err.content)
+
     except AccessTokenRefreshError:
         print ("Credentials have been revoked or expired, please re-run"
          "the application to re-authorize")
