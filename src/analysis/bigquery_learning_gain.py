66b9e45b8572e602637ef7d82b5901f365b61f86
Tony Liu <tony@khanacademy.org>
2014-02-19 18:52:53 -0800

    Refactor BigQuery code
    Test Plan:
    WIP

--- src/analysis/bigquery_learning_gain.py
+++ src/analysis/bigquery_learning_gain.py
@@ -5,6 +5,7 @@ This also serves as an experiment in using the BigQuery Python API.
 """
 
 import httplib2
+import os
 import pprint
 
 from apiclient.discovery import build
@@ -16,38 +17,48 @@ from oauth2client.file import Storage
 from oauth2client.tools import run
 
 
+PROJECT_ID_FILE = 'id.txt'
+CREDENTIALS_FILE = 'bigquery_credentials.dat'
+CLIENT_SECRETS_FILE = os.path.expanduser('~/khan/clients_secrets.json')
+
+
 def get_project_id():
     # The project ID lives in id.txt (to avoid having this in plaintext)
-    f = open('id.txt', 'r')
-    return f.readline().strip()
-
-# Enter your Google Developer Project number
-PROJECT_NUMBER = get_project_id()
-FLOW = flow_from_clientsecrets('/Users/tony/khan/client_secrets.json',
-                               scope='https://www.googleapis.com/auth/bigquery')
+    f = open(PROJECT_ID_FILE, 'r')
+    project_id = f.readline().strip()
+    return project_id
 
 
-def main():
-    storage = Storage('bigquery_credentials.dat')
+def get_bigquery_service():
+    storage = Storage(CREDENTIALS_FILE)
     credentials = storage.get()
 
     if credentials is None or credentials.invalid:
-        credentials = run(FLOW, storage)
+        flow = flow_from_clientsecrets(
+            CLIENT_SECRETS_FILE,
+            scope='https://www.googleapis.com/auth/bigquery'
+        )
+        credentials = run(flow, storage)
 
     http = httplib2.Http()
     http = credentials.authorize(http)
 
     bigquery_service = build('bigquery', 'v2', http=http)
 
+    return bigquery_service
+
+
+def main():
+    bigquery_service = get_bigquery_service()
+
     try:
         datasets = bigquery_service.datasets()
-        listReply = datasets.list(projectId=PROJECT_NUMBER).execute()
+        project_id = get_project_id()
+        list_reply = datasets.list(projectId=project_id).execute()
         print 'Dataset list:'
-        pprint.pprint(listReply)
-
+        pprint.pprint(list_reply)
     except HttpError as err:
         print 'Error in listDatasets:', pprint.pprint(err.content)
-
     except AccessTokenRefreshError:
         print ("Credentials have been revoked or expired, please re-run"
          "the application to re-authorize")
@@ -55,4 +66,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
