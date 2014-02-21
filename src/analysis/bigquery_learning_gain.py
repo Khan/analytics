2970a95969a417c8a33e9b8ec133e56c3096fd97
Tony Liu <tony@khanacademy.org>
2014-02-20 17:37:52 -0800

    Get start date for A/B test via query
    Test Plan:
    WIP

--- src/analysis/bigquery_learning_gain.py
+++ src/analysis/bigquery_learning_gain.py
@@ -12,6 +12,7 @@ Setup notes:
     python bigquery_learning_gain.py
 """
 
+import datetime
 import httplib2
 import os
 import pprint
@@ -99,6 +100,7 @@ def run_query(service, project_id, query_string,
                 break
             else:
                 time.sleep(1.0)
+        return query_response
 
     except HttpError as err:
         print 'Error:', pprint.pprint(err.content)
@@ -108,8 +110,50 @@ def run_query(service, project_id, query_string,
          "the application to re-authorize")
 
 
+def get_start_date_for_experiment(service, project_id, experiment_name,
+                                  backup_date):
+    # Note that we can't reliably get the date from GAE Bingo since there's
+    # a bug when new conversions are added and they are first alphabetically.
+    # Instead, let's just run a query...
+
+    # We assume that the earliest conversion (that hasn't changed) is
+    # actually wne the A/B test started. Reasonable.
+    query_request = service.jobs()
+    query_string = (
+        'SELECT MIN(dt_started) as start_date\n'
+        'FROM [%s._GAEBingoExperiment]\n'
+        'WHERE canonical_name = \'%s\'\n'
+        'GROUP BY canonical_name\n'
+    ) % (backup_date, experiment_name)
+
+    query_response = query_request.query(projectId=project_id,
+        body={
+            'query': query_string
+        }
+    ).execute()
+
+    for row in query_response['rows']:
+        for field in row['f']:
+            usec = int(field['v'])
+            ts = usec / 1000 / 1000
+            start_date = datetime.datetime.fromtimestamp(ts)
+            return start_date
+    # Should never get here!
+    raise
+
+
 def generate_learning_gain(service, project_id, experiment_name,
-                           start_date, backup_date):
+                           backup_date):
+    # Get start date from experiment name
+    start_date = get_start_date_for_experiment(service, project_id,
+        experiment_name, backup_date)
+
+    print '\nRunning learning gain...'
+    print 'Bucket: %s\n' % backup_date
+
+    print experiment_name
+    print 'Started: %s' % start_date
+
     # Query 1:
     # TODO(tony): generate analytics_cards_1 table (rename?)
     # Note: only need to do this once (and it's the most expensive)
@@ -119,7 +163,7 @@ def generate_learning_gain(service, project_id, experiment_name,
         'SELECT user_id, correct, time_done\n'
         'FROM [tony.analytics_cards_1]\n'
         'WHERE task_type = \'mastery.analytics\'\n'
-        'AND time_done >= TIMESTAMP(\'%s 00:00:00\')\n'
+        'AND time_done >= TIMESTAMP(\'%s\')\n'
     ) % start_date
     run_query(service, project_id, query_string, 'exp_analytics')
 
@@ -172,18 +216,26 @@ def generate_learning_gain(service, project_id, experiment_name,
     )
     run_query(service, project_id, query_string, 'exp_results')
 
+    # Wrap it up
+    print '\nResults ready!\n'
+    print experiment_name
+    print 'Started: %s (%s days old)' % (start_date, 'TODO')
+    # TODO(tony): print results here too
+
 
 def main():
     service = get_bigquery_service()
     project_id = get_project_id()
     generate_learning_gain(service, project_id,
-        # 'Review scheduling methods', '2013-11-20',
+        'Review scheduling methods',
         # 'adaptive pretest question difficulty cutoff', '2013-10-19',
         # 'Athena: mastery task length v2', '2013-10-22',
+        # 'Mastery Tasks: Challenge Card Enforce Prerequisites', '2013-11-18',
+        # 'Mastery Tasks: Challenge Card Selection Aggressiveness',
         # 'Mastery Tasks: progress card ordering', '2013-11-28',
         # 'metacognitive 2', '2013-09-14',
         # 'metacognitive 2 prompt type', '2013-09-14',
-        'metacognitive 2 text', '2013-09-14',
+        # 'metacognitive 2 text', '2013-09-14',
         '2014_02_08')
 
 
