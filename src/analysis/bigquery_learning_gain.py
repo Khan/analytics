c1f00f16130a345c9228d43ee9288a051cc1d711
Tony Liu <tony@khanacademy.org>
2014-02-24 19:31:49 -0800

    Switch to normal distribution; add first/last accuracies
    Test Plan:
    WIP

--- src/analysis/bigquery_learning_gain.py
+++ src/analysis/bigquery_learning_gain.py
@@ -124,7 +124,7 @@ def get_start_date_for_experiment(service, project_id, experiment_name,
 
     # We assume that the earliest conversion (that hasn't changed) is
     # actually we the A/B test started. Reasonable.
-    print 'Getting start date for experiment', experiment_name
+    print 'Getting start date for experiment:', experiment_name
     query_request = service.jobs()
     query_string = (
         'SELECT MIN(dt_started) as start_date\n'
@@ -289,6 +289,8 @@ def generate_learning_gain(service, project_id, backup_date,
         '  ROUND(100 * AVG(last_correct - first_correct), 5) AS delta,\n'
         '  ROUND(100 * STDDEV(last_correct - first_correct) / SQRT(n), 5)\n'
         '    as stderr,\n'
+        '  ROUND(100 * AVG(first_correct), 3) as first_correct,\n'
+        '  ROUND(100 * AVG(last_correct), 3) as last_correct,\n'
         '  COUNT(alternative) AS n\n'
         'FROM [tony.exp_analytics_first_last]\n'
         'WHERE alternative != "null"\n'
@@ -309,17 +311,16 @@ def generate_learning_gain(service, project_id, backup_date,
     return results
 
 
-def calculate_t_test_cdf(n1, n2, m1, m2, s1, s2):
+def calculate_prob(n1, n2, m1, m2, s1, s2):
     # Calculates the probability that the first sample's mean is greater than
     # the second. We assume equal variances but unequal sample sizes. We use
-    # a t-test here. Note that we can't use the built in one from scipy because
-    # we don't have the whole sample available to us (just means and stderr,
-    # which is all we need). So, we compute it directly.
-    s12 = sqrt(((n1 - 1) * s1 * s1 + (n2 - 1) * s2 * s2) / (n1 + n2 - 2))
+    # a z-test here (the sample sizes are huge; same as t-test).
+    s12 = sqrt(((n1 - 1) * s1 * s1 * n1
+              + (n2 - 1) * s2 * s2 * n2) / (n1 + n2 - 2))
     print 's12:', s12
-    t = 1.0 * (m1 - m2) / (s12 * sqrt(1.0 / n1 + 1.0 / n2))
-    print 't:', t
-    p = stats.t.cdf(t, n1 + n2 - 2)
+    z = 1.0 * (m1 - m2) / (s12 * sqrt(1.0 / n1 + 1.0 / n2))
+    print 'z:', z
+    p = stats.norm.cdf(z)
     print 'p:', p
     return p
 
@@ -337,15 +338,16 @@ def generate_report(backup_date, all_results):
         text += 'Started: %s (%d days old)\n' % (results.start_date.date(),
                                                results.days_old)
         text += '\n' + str(data) + '\n\n'
-        text += 'The best alternative is: %s!\n' % best_alternative
+        text += 'The best alternative is [%s]!\n' % best_alternative
         for j in xrange(len(data)):
             if j == i:
                 continue
             n1, n2 = data['n'][i], data['n'][j]
             m1, m2 = data['delta'][i], data['delta'][j]
             s1, s2 = data['stderr'][i], data['stderr'][j]
-            prob = calculate_t_test_cdf(n1, n2, m1, m2, s1, s2)
-            text += ('The probability this is better than %s is %.2f%%.\n' % (
+            prob = calculate_prob(n1, n2, m1, m2, s1, s2)
+            text += ('The probability this is better than [%s]'
+                     ' is %.2f%%.\n' % (
                      data['alternative'][j], prob * 100.0))
 
         print text
@@ -371,7 +373,7 @@ def main():
         # 'Mastery Tasks: Challenge Card Enforce Prerequisites',
         # 'Mastery Tasks: Challenge Card Selection Aggressiveness',
         # 'Mastery Tasks: progress card ordering',
-        'metacognitive 2',
+        # 'metacognitive 2',
         # 'metacognitive 2 prompt type',
         # 'metacognitive 2 text',
 
@@ -380,7 +382,7 @@ def main():
         # 'Practice Tasks: Suggestion Strategy',
 
         # 'pretest utility function - difficulty',
-        # 'pretest utility function - time',
+        'pretest utility function - time',
         # 'Pretest: parameterization',
         # 'Pretest: promoter aggressiveness',
     ]
